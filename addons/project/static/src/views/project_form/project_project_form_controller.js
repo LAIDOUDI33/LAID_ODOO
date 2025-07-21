aa0b1ab221b45680b5e1a3fd8d5014f2398c8ea4
dhtr-odoo <dhtr@odoo.com>
2025-07-21 15:07:43 +0530

    [IMP] hr_timesheet, _*: confirm timesheet disabling on analytic account removal
    _*=project
    Before this commit:
    When removing an analytic account from a project that has timesheets enabled:
    - The "Allow timesheets" field was automatically disabled
    - This change happened silently, without any warning or confirmation
    - Users were unknowingly disabling timesheet tracking, causing confusion and
    potential data loss
    
    After this commit:
    - A confirmation dialog is displayed before disabling timesheets
    - Users must explicitly confirm their intention to proceed
    - Canceling the dialog prevents the account removal
    - The dialog clearly explains that removing the analytic account will disable
    the timesheets feature
    
    task-4593133

--- addons/project/static/src/views/project_form/project_project_form_controller.js
+++ addons/project/static/src/views/project_form/project_project_form_controller.js
@@ -2,6 +2,8 @@ import { useLayoutEffect } from "@web/owl2/utils";
 import { onWillStart, props, t } from "@odoo/owl";
 import { user } from "@web/core/user";
 import { formControllerProps } from "@web/views/form/form_controller";
+import { _t } from "@web/core/l10n/translation";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { FormControllerWithHTMLExpander } from '@resource/views/form_with_html_expander/form_controller_with_html_expander'
 import { ProjectTemplateDropdown } from "../components/project_template_dropdown";
 
@@ -39,6 +41,35 @@ export class ProjectProjectFormController extends FormControllerWithHTMLExpander
                 () => [this.rootRef.el]
             );
         }
+    }
+
+    async onWillSaveRecord(record, changes) {
+        const prevAccountId = record._values.account_id?.id;
+        const newAccountId = "account_id" in changes ? changes.account_id : prevAccountId;
+        const timesheetsEnabled = "allow_timesheets" in changes
+            ? changes.allow_timesheets
+            : record.data.allow_timesheets ?? false;
+        const isAccountRemoved = !!prevAccountId && !newAccountId;
+        if (isAccountRemoved && timesheetsEnabled) {
+            const confirmed = await new Promise(resolve => {
+                this.dialogService.add(ConfirmationDialog, {
+                    title: _t("Warning"),
+                    body: _t(
+                        "The Timesheets feature requires an analytic account for the Project plan. Removing it will disable the feature. Are you sure you want to continue?"
+                    ),
+                    confirmLabel: _t("Proceed"),
+                    cancelLabel: _t("Cancel"),
+                    confirm: () => resolve(true),
+                    cancel: () => resolve(false),
+                });
+            });
+            if (confirmed) {
+                changes.allow_timesheets = false;
+            } else {
+                return false;
+            }
+        }
+        return super.onWillSaveRecord(...arguments);
     }
 
     getStaticActionMenuItems() {
