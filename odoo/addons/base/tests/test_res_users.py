50dc2aa8dcaa6a96a1da81063fe25fc9e0cc73b5
Rémy Voet (ryv) <ryv@odoo.com>
2025-12-02 10:27:41 +0100

    [FIX] *: update query count

--- odoo/addons/base/tests/test_res_users.py
+++ odoo/addons/base/tests/test_res_users.py
@@ -533,8 +533,8 @@ class TestUsers2(UsersCommonCase):
         # Process any tracking message at flush for cleaner queryCount
         self.flush_tracking()
 
-        # all modules: 37, base: 9; nightly: +1
-        with self.assertQueryCount(38):
+        # all modules: 15, base: 10; nightly: +1
+        with self.assertQueryCount(16):
             self.user_internal.write({
                 "group_ids": [Command.link(contact_creation_group.id)],
             })
