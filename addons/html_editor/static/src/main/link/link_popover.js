80ff4429057464698103967e0d2c794fc96971f2
Benoit Socias (bso) <bso@odoo.com>
2026-03-06 15:13:17 +0100

    [IMP] html_editor: display the link popover in a bottom sheet in mobile
    This commit makes the link popover open in a bottom sheet when using
    mobile mode.
    
    task-6009844

--- addons/html_editor/static/src/main/link/link_popover.js
+++ addons/html_editor/static/src/main/link/link_popover.js
@@ -4,6 +4,7 @@ import { session } from "@web/session";
 import { _t } from "@web/core/l10n/translation";
 import { Component, props, proxy, signal, t, useEffect } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
+import { scrollTo } from "@web/core/utils/scrolling";
 import { cleanZWChars, deduceURLfromText } from "./utils";
 import { CheckBox } from "@web/core/checkbox/checkbox";
 import { isAbsoluteURLInCurrentDomain } from "@html_editor/utils/url";
@@ -47,6 +48,7 @@ export const linkPopoverProps = {
     allowStripDomain: t.boolean().optional(),
     publicAttachments: t.boolean().optional(),
     advancedAttributeOptions: t.array().optional(),
+    close: t.function().optional(),
 };
 
 export class LinkPopover extends Component {
@@ -183,6 +185,7 @@ export class LinkPopover extends Component {
     discard() {
         this.props.onDiscard();
         this.cancelUpload?.();
+        this.props.close?.();
     }
 
     onChange() {
@@ -196,6 +199,7 @@ export class LinkPopover extends Component {
         this.applyDeducedUrl();
         const params = this.prepareLinkParams();
         this.props.onApply(params);
+        this.props.close?.();
     }
     applyDeducedUrl() {
         if (this.state.label === "") {
@@ -220,6 +224,12 @@ export class LinkPopover extends Component {
         this.state.editing = true;
         this.props.onEdit();
         this.updateUrlAndLabel();
+        setTimeout(() => {
+            // Once rendered, make bottom sheet fully visible:
+            if (this.editingWrapper.el) {
+                scrollTo(this.editingWrapper.el);
+            }
+        });
     }
     updateUrlAndLabel() {
         this.state.url = this.props.linkElement.getAttribute("href");
@@ -289,6 +299,18 @@ export class LinkPopover extends Component {
     onSelectedLinkType(type) {
         this.state.type = type;
         this.onChange();
+    }
+
+    /**
+     * Called when the preview image is loaded.
+     *
+     * @param {Event} ev
+     */
+    onImageLoaded(ev) {
+        // On mobile, we want to scroll the bottom sheet up to display the whole preview.
+        if (ev.target.closest(".o_bottom_sheet")) {
+            scrollTo(ev.target, { behavior: "smooth" });
+        }
     }
 
     /**
