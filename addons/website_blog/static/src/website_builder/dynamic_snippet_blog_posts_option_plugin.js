0bcd654e33493c2f0263afb860931da905f5b2c4
Sébastien (blse) <blse@odoo.com>
2026-03-30 08:07:27 +0200

    [IMP] website, *: render dynamic snippets on the server
    *: website_blog, website_event, website_sale
    
    A dynamic snippet is composed of 3 templates:
    - The snippet, containing the header, the mains rows, ...
    - The grid or carousel, grouping the items
    - The items, containing the detail of each fetched entries
    
    Before this commit, the page returned by the server contained the
    snippet part, with a placeholder for the content. An interaction
    (running in the browser) would react to this structure and do the
    following:
    - gather information from the snippet or the page about what content
      should be shown
    - send some of that information to the server to fetched the rendered
      items
    - render the "grid" with those items, and insert that at the placeholder
      for the dynamic snippet content
    
    After this commit, the page returned by the server contains everything.
    The content is rendered with the directive `t-shared-snippet` calling
    the new template `website.dynamic_snippet_content`.
    
    This change has several implications that constitutes most of this commit:
    - The "grid" template is rendered on the server, thus their
      definition is moved and adapted (`website.s_dynamic_snippet.grid`,
      `website.s_dynamic_snippet.carousel` and its parts,
      `website_sale.s_dynamic_snippet_category.grid`).
    - The rendering of the "grid" template has now no information about the
      size of the viewport, which was used to change the number of element
      shown per row. Thus `wrap_dynamic_snippet` is added to rewrap those
      rows to the desired number of items (passing this information adds
      even more changes to the "grid" templates)
    - The parameters for the dynamic content are passed throught `data-arg-`
      attributes on the element with `t-shared-snippet`, instead of being
      gathered/computed by an interaction. This changes a lot for the
      options that were modifying values read by those interactions:
      - Some parameters are just moved to the `t-shared-snippet` node
      - Some computation is moved to the template/filter, thus on the server
        side (and the arguments for that computation are passed through
        `data-arg-`, or are the `mainObject`)
      - Some computation id moved to the options, and the computed value
        is stored in the dom (in a `data-arg-`)
    
    As this centralizes the parameters in the `data-arg-*` attributes of
    the content node (with `t-shared-snippet`), most default values can now
    easily be added in the snippet template instead of being set on the drop
    of the snippet (which this commit does as well).
    
    The customization of dynamic snippet by the configurator is also adapted
    to edit the `data-arg-*`.
    
    task-5245362

--- addons/website_blog/static/src/website_builder/dynamic_snippet_blog_posts_option_plugin.js
+++ addons/website_blog/static/src/website_builder/dynamic_snippet_blog_posts_option_plugin.js
@@ -1,31 +1,16 @@
 import { Plugin } from "@html_editor/plugin";
 import { registry } from "@web/core/registry";
 
-/**
- * @typedef { Object } DynamicSnippetBlogPostsOptionShared
- * @property { DynamicSnippetBlogPostsOptionPlugin['getModelNameFilter'] } getModelNameFilter
- */
-
 export class DynamicSnippetBlogPostsOptionPlugin extends Plugin {
     static id = "dynamicSnippetBlogPostsOption";
-    static dependencies = ["dynamicSnippetOption"];
-    static shared = ["getModelNameFilter"];
-    modelNameFilter = "blog.post";
     /** @type {import("plugins").WebsiteResources} */
     resources = {
-        on_snippet_dropped_handlers: this.onSnippetDropped.bind(this),
+        model_name_filter_overrides: (snippetEl) => {
+            if (snippetEl.matches(".s_dynamic_snippet_blog_posts")) {
+                return "blog.post";
+            }
+        },
     };
-    getModelNameFilter() {
-        return this.modelNameFilter;
-    }
-    async onSnippetDropped({ snippetEl }) {
-        if (snippetEl.matches(".s_dynamic_snippet_blog_posts")) {
-            await this.dependencies.dynamicSnippetOption.setOptionsDefaultValues(
-                snippetEl,
-                this.modelNameFilter
-            );
-        }
-    }
 }
 
 registry
