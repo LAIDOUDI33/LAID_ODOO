3d57879a628efcc103028b0ca936e502ff7e85af
Jeanne Delneste <jedel@odoo.com>
2026-05-18 08:09:57 +0200

    [IMP] l10n_pl: split payment mechanism
    For Poland, when the invoice/bill is over 15K PLN or if it contains sensitive
    goods, it should include a MPP (splt payment) mention.
    We added on the product or product category the fiels `l10n_pl_subject_to_split_payment`
    to label these sensitive goods.
    
    We let the possibility to the user to uncheck the MPP checkbox.
    
    We also added the mention in the invoice pdf.
    
    task-5935840

--- addons/l10n_pl/models/account_move.py
+++ addons/l10n_pl/models/account_move.py
@@ -1,5 +1,7 @@
 from odoo import api, fields, models
 
+MAX_AMOUNT_MPP = 15000
+
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -16,6 +18,24 @@ class AccountMove(models.Model):
         string='B_MPV_Prowizja',
         help="Supply of agency and other services pertaining to the transfer of a single-purpose voucher",
     )
+    l10n_pl_mpp = fields.Boolean(
+        string='MPP',
+        compute='_compute_l10n_pl_mpp',
+        inverse='_inverse_l10n_pl_mpp',
+        store=True, copy=False,
+        help="Split Payment Mechanism, indicates if a split payment is necessary",
+    )
+    l10n_pl_mpp_mode = fields.Selection([
+        ('auto', 'Automatic'),
+        ('manual', 'Manual'),
+    ], default='auto', copy=False,
+    )
+    l10n_pl_show_mpp = fields.Boolean(
+        compute='_compute_l10n_pl_show_mpp',
+    )
+    l10n_pl_show_mpp_warning = fields.Boolean(
+        compute='_compute_l10n_pl_show_mpp_warning',
+    )
 
     @api.depends('country_code')
     def _compute_show_taxable_supply_date(self):
@@ -28,6 +48,55 @@ class AccountMove(models.Model):
         super()._compute_taxable_supply_date_placeholder()
         for move in self.filtered(lambda m: m.country_code == 'PL'):
             move.taxable_supply_date_placeholder = self.env._("Invoice Date")
+
+    @api.depends(
+        'l10n_pl_mpp_mode',
+        'l10n_pl_show_mpp',
+        'commercial_partner_id.is_company',
+        'invoice_line_ids.price_subtotal',
+        'invoice_line_ids.currency_rate',
+        'invoice_line_ids.product_id',
+    )
+    def _compute_l10n_pl_mpp(self):
+        for move in self:
+            if move.l10n_pl_mpp_mode == 'manual':
+                continue
+            move.l10n_pl_mpp = move._is_mpp_mandatory()
+
+    def _inverse_l10n_pl_mpp(self):
+        for move in self:
+            move.l10n_pl_mpp_mode = 'manual'
+
+    def _is_mpp_mandatory(self):
+        """
+        The field MPP is mandatory if those 3 conditions are satisfied:
+            * The transaction is B2B
+            * The gross total reaches PLN 15,000 (=MAX_AMOUNT_MPP)
+            * The invoice includes at least one 'subject_to_split_payment' product
+        """
+        return (
+            self.l10n_pl_show_mpp and
+            self.commercial_partner_id.is_company and
+            sum(line.price_subtotal / (line.currency_rate or 1.0) for line in self.invoice_line_ids) >= MAX_AMOUNT_MPP and
+            any(
+                line.product_id.l10n_pl_subject_to_split_payment or
+                line.product_id.categ_id.l10n_pl_subject_to_split_payment
+                for line in self.invoice_line_ids
+            )
+        )
+
+    @api.depends('country_code', 'commercial_partner_id.country_code')
+    def _compute_l10n_pl_show_mpp(self):
+        for move in self:
+            move.l10n_pl_show_mpp = move.country_code == 'PL' and move.commercial_partner_id.country_code == 'PL'
+
+    @api.depends('l10n_pl_mpp')
+    def _compute_l10n_pl_show_mpp_warning(self):
+        """
+        Warning if MPP not checked when it is mandatory
+        """
+        for move in self:
+            move.l10n_pl_show_mpp_warning = move._is_mpp_mandatory() and not move.l10n_pl_mpp
 
     def _get_accounting_date_source(self):
         return (self.country_code == 'PL' and self.taxable_supply_date) or super()._get_accounting_date_source()
