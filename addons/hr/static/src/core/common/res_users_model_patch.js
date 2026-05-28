6c186b6393114f1ec5d90e29132102b9baeb17c7
maad-odoo <maad@odoo.com>
2026-05-28 16:11:11 +0530

    [FIX] hr*: load out-of-office dates from all user employees
    *=hr_holidays,im_livechat,mail,test_discuss_full
    
    Out-of-office return dates were loaded through the partner's main user's
    employee_ids. That relation is company-filtered, so users without access to
    the employee's company did not receive leave_date_to in Discuss until
    opening the avatar card refreshed the data through another path.
    
    This commit fixes this behavior by loading leave_date_to from all employees
    linked to the partner's main user using sudo and exposing them through the
    all_employee_ids store relation. This makes the out-of-office indication
    consistently available in the sidebar, member list, and chat banner.
    
    task-6095661

--- addons/hr/static/src/core/common/res_users_model_patch.js
+++ addons/hr/static/src/core/common/res_users_model_patch.js
@@ -1,6 +1,7 @@
 import { patch } from "@web/core/utils/patch";
 import { fields } from "@mail/model/misc";
 import { ResUsers } from "@mail/core/common/res_users_model";
+import { user } from "@web/core/user";
 
 patch(ResUsers.prototype, {
     setup() {
@@ -8,9 +9,15 @@ patch(ResUsers.prototype, {
         this.employee_ids = fields.Many("hr.employee", {
             inverse: "user_id",
         });
+        this.all_employee_ids = fields.Many("hr.employee");
         this.employee_id = fields.One("hr.employee", {
             compute() {
-                return this.store.getRelevantEmployee(this.employee_ids);
+                const employees = this.all_employee_ids.length
+                    ? this.all_employee_ids
+                    : this.employee_ids;
+                return employees.find(
+                    (employee) => employee.company_id?.id === user.activeCompany.id
+                );
             },
         });
     },
