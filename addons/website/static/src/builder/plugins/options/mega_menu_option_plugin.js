7075a166e4045e82c3ef10577cc948e7d02e608b
livaios <liew@odoo.com>
2026-06-17 14:23:54 +0200

    [IMP] website,website_sale: Mega Menu Rework
    1. Remove "+ mega menu item" and make it a checkbox on menu item instead
    2. Add dropzones before & after each template
    3. Remove size on the editor for mega menu and rely on general layout
    4. Add a blank template last
    
    task-6171291

--- addons/website/static/src/builder/plugins/options/mega_menu_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/mega_menu_option_plugin.js
@@ -1,23 +1,23 @@
 import { Plugin } from "@html_editor/plugin";
 import { registry } from "@web/core/registry";
 
-/**
- * @typedef { Object } MegaMenuOptionShared
- * @property { MegaMenuOptionPlugin['getTemplatePrefix'] } getTemplatePrefix
- */
-
 export class MegaMenuOptionPlugin extends Plugin {
     static id = "megaMenuOptionPlugin";
-    static dependencies = [];
-    static shared = ["getTemplatePrefix"];
 
     /** @type {import("plugins").WebsiteResources} */
     resources = {
-        dropzone_selectors: {
-            selector: ".o_mega_menu .nav > .nav-link",
-            dropIn: ".o_mega_menu nav",
-            dropNear: ".o_mega_menu .nav-link",
-        },
+        dropzone_selectors: [
+            {
+                selector: ".o_mega_menu .nav > .nav-link",
+                dropIn: ".o_mega_menu nav",
+                dropNear: ".o_mega_menu .nav-link",
+            },
+            {
+                // Snippets excluded from being dropped in the Mega Menu.
+                selector: ".s_popup, .s_whatsapp",
+                excludeAncestor: ".o_mega_menu",
+            },
+        ],
         on_ready_to_save_document_handlers: this.saveMegaMenuClasses.bind(this),
         no_parent_containers: ".o_mega_menu",
         is_unremovable_selectors: ".o_mega_menu > section",
@@ -30,10 +30,15 @@ export class MegaMenuOptionPlugin extends Plugin {
                 return false;
             }
         },
+        on_element_dropped_handlers: this.reopenMegaMenu.bind(this),
+        on_snippet_dropped_handlers: this.reopenMegaMenu.bind(this),
     };
 
-    getTemplatePrefix() {
-        return "website.";
+    reopenMegaMenu() {
+        const megaMenuToggleEl = this.editable.querySelector(".o_mega_menu_toggle")
+        if (megaMenuToggleEl && !megaMenuToggleEl.classList.contains("show")) {
+            window.Dropdown.getOrCreateInstance(megaMenuToggleEl).show();
+        }
     }
 
     async saveMegaMenuClasses() {
