18faeae6bd5b862607aeac047bac4976c6bf13be
soza-odoo <soza@odoo.com>
2026-06-26 10:43:40 +0530

    [IMP] l10n_cz:  Variable Code and Bank Reconciliation
    In this commit:
    - While downloading the bank transactions, we identify the  'Variable code' in
      incoming transaction data and store it in the bank entry.
    - On the bank reconciliation widget, we will  add 'Variable code' in front
      of the label.
    
    task-6030605

---
+++ addons/l10n_cz/tests/test_import_bank_statement.py
@@ -0,0 +1,50 @@
+from odoo import fields
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+from odoo.tests import tagged
+
+
+@tagged('post_install_l10n', 'post_install', '-at_install')
+class TestAccountBankStatementImport(AccountTestInvoicingCommon):
+
+    @classmethod
+    @AccountTestInvoicingCommon.setup_country('cz')
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_cz_bank_transaction_with_variable_symbol(self):
+        transaction_with_vs = {
+            'online_transaction_identifier': 'transaction_1',
+            'date': fields.Date.to_string(fields.Date.today()),
+            'payment_ref': 'Office rent',
+            'l10n_cz_variable_symbol': '202600111',
+            'amount': 10.0,
+        }
+        transaction_with_details = {
+            'online_transaction_identifier': 'transaction_2',
+            'date': fields.Date.to_string(fields.Date.today()),
+            'payment_ref': 'Last Year Interests',
+            'amount': 12.0,
+            'transaction_details': {
+                'amount': 12.0,
+                'partner_name': 'ABC',
+                'extra': {
+                    'variable_code': '202600222',
+                },
+            },
+        }
+
+        bsl_with_vs, bsl_with_details = self.env['account.bank.statement.line'].with_context(
+            skip_statement_line_cron_trigger=True,
+        ).create([
+            transaction_with_vs,
+            transaction_with_details,
+        ])
+
+        self.assertEqual(
+            bsl_with_vs.payment_ref,
+            '202600111 - Office rent',
+        )
+        self.assertEqual(
+            bsl_with_details.payment_ref,
+            '202600222 - Last Year Interests',
+        )
