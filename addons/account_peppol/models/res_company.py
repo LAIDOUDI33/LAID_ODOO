27968ebaabbeba631428aaa320f5badfea8b1cc8
jond-odoo <jond@odoo.com>
2026-07-11 02:34:58 +0200

    [IMP] account_peppol: Add MLS messages to Peppol
    Message Level Status (MLS) messages must be handled from 2027
    on the Peppol Network.
    We must be able to send and receive acknowledgment, approval
    and rejection MLS messages.
    
    task-id: 6377775

--- addons/account_peppol/models/res_company.py
+++ addons/account_peppol/models/res_company.py
@@ -327,6 +327,8 @@ class ResCompany(models.Model):
                     "Peppol BIS Self-Billing UBL Invoice V3",
                 "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:selfbilling:3.0::2.1":
                     "Peppol BIS Self-Billing UBL CreditNote V3",
+                "urn:oasis:names:specification:ubl:schema:xsd:ApplicationResponse-2::ApplicationResponse##urn:peppol:edec:mls:1.0::2.1":
+                    "Peppol Message Level Status v1.0",
             }
         }
 
