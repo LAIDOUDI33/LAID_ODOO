odoo/odoo

commit af36e73108cef4326f6125c3af491b07a51534fe

[FIX] resource, hr_work_entry_contract_attendance : Fetch attendance for flexible since start_date not monday
Steps to reproduce:
	- Set Marc Demo's contract work entry source to attendances and working schedule to flexible hours.
	- Create a public holiday with generic time off work entry type.
	- Create one or multiple attendances for marc demo on the public holiday.
	- Regenerate work entries for marc demo for that day, the gaps in between
	the attendances created and the working hours will be filled with work entries
	with the right start/end time but duration will always be 8h.

Cause:
This is happening because when getting the duration batch for the work entry
we get the attendance intervals the employee should work in that period
and if the employee is flexible we will get a fake attendance with the
number of hours required per day ignoring if the period is just a small
period of the day

Fix:
We are checking now since the start date not monday so we don't set
a fixed week start. We check if the period is less than the remaining hours
we get it as it mostly means that it is less than one day

opw-4887933

closes odoo/odoo#218823

Related: odoo/enterprise#90163
Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

addons/resource/models/resource_calendar.py
@@ -391,21 +391,22 @@ class ResourceCalendar(models.Model):
                     max_hours_per_day = resource.calendar_id.hours_per_day
 
                     intervals = []
-                    current_monday = start_date - timedelta(days=start_date.weekday())
+                    current_start_day = start_date
 
-                    while current_monday <= end_date:
-                        current_sunday = current_monday + timedelta(days=6)
+                    while current_start_day <= end_date:
+                        current_end_of_week = current_start_day + timedelta(days=6)
 
-                        week_start = max(current_monday, start_date)
-                        week_end = min(current_sunday, end_date)
+                        week_start = max(current_start_day, start_date)
+                        week_end = min(current_end_of_week, end_date)
 
-                        if current_monday < start_date:
-                            prior_days = (start_date - current_monday).days
+                        if current_start_day < start_date:
+                            prior_days = (start_date - current_start_day).days
                             prior_hours = min(full_time_required_hours, max_hours_per_day * prior_days)
                         else:
                             prior_hours = 0
 
                         remaining_hours = max(0, full_time_required_hours - prior_hours)
+                        remaining_hours = min(remaining_hours, (end_dt - start_dt).total_seconds() / 3600)
 
                         current_day = week_start
                         while current_day <= week_end:
@@ -427,7 +428,7 @@ class ResourceCalendar(models.Model):
 
                             current_day += timedelta(days=1)
 
-                        current_monday += timedelta(days=7)
+                        current_start_day += timedelta(days=7)
 
                     result_per_resource_id[resource.id] = WorkIntervals(intervals)
                 elif resource in per_resource_result:

addons/hr_holidays/tests/test_leave_requests.py
@@ -1251,8 +1251,8 @@ class TestLeaveRequests(TestHrHolidaysCommon):
             'request_date_to': '2024-01-27',
         })
         holiday_status = self.holidays_type_4.with_user(self.user_employee_id)
-        self._check_holidays_status(holiday_status, employee, 20.0, 0.0, 20.0, 16.0)
-        self.assertEqual(leave.duration_display, '4 days')
+        self._check_holidays_status(holiday_status, employee, 20.0, 0.0, 20.0, 15.0)
+        self.assertEqual(leave.duration_display, '5 days')
 
     def test_default_request_date_timezone(self):
         """

LAIDOUDI33/LAID_ODOO

commit f131ea5fbefdc697bd690395557eac9584487945

[FIX] resource: start gantt remaining hours computation on Mondays
## Issue
When browsing the gantt view of monthly attendances and looking at the
progress bar of an employee with a flexible schedule, the amount of "expected"
hours does not match the amount of hours expected for a similar, fixed schedule.

## Steps to reproduce
1. Install *Attendances* (`hr_attendance`)
2. For an Employee E:
    - Set their Working Hours to a Flexible 40 hours/week schedule, averaging
    8 hours per day
3. In Attendances, set the range to *This Month* and go back to May 2026
4. **For Employee E, the progress bar (shown when hovering the employee)
shows 184 hours, but for other employees with a fixed schedule, it only
shows 168 hours, which is in fact 21 days * 8 hours.**

## Cause
The issue was introduced by https://github.com/odoo/odoo/commit/af36e73108ce.
Instead of counting the amount of expected hours by starting on the first
Monday, we start counting on the first of the month, and we start counting
5 days by 5, regardless of weekends.

https://github.com/odoo/odoo/blob/f0fb287c650285f5d7ded7d2cf12b0c5cccc21ca/addons/resource/models/resource_calendar.py#L397-L400

In the above snippet of code, the `start_date` is the first date on the
gantt view.

Before commit https://github.com/odoo/odoo/commit/af36e73108ce, we would
take the first Monday before the requested start ** and compute the end
of the week based on that Monday**.

https://github.com/odoo/odoo/blob/6bf75265a4c2d1d59105fa34f035d89fe1fd8acb/addons/resource/models/resource_calendar.py#L394-L399

## Fix
We partially revert the behavior introduced by the previous commit by
counting expected hours starting on a Monday when displaying the Gantt
view.

opw-6369287

addons/resource/models/resource_calendar.py
@@ -426,6 +426,8 @@ class ResourceCalendar(models.Model):
 
                     intervals = []
                     current_start_day = start_date
+                    if self.env.context.get('gantt_start_date'):
+                        current_start_day -= timedelta(days=start_date.weekday())
 
                     while current_start_day <= end_date:
                         current_end_of_week = current_start_day + timedelta(days=6)