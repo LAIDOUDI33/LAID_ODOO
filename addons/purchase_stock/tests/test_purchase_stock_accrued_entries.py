odoo/odoo

commit 81f25bc57b8433a65bf33950c64dc7582240a229

[IMP] *: rework accrual entries for PO/SO
*: account, pos_sale, purchase, purchase_mrp, purchase_stock, sale,
sale_mrp, sale_project, sale_stock, sale_timesheet, stock_account

This commit modifies `sale.order.line` and `purchase.order.line` models
to work with the new accrual reports.

List of changes made:

Split compute methods
=====================
The `purchase.order.line` has two fields, `qty_received` and
`qty_invoiced`, and we would like to be able to know what was those
fields value in the past at a given date. The `sale.order.line` has also
two fields in the same situation, `qty_delivered` and `qty_invoiced`.

Currently, those fields are computed and stored, thus we need a
computed non-stored version of those fields to be able to re-compute
their value depending of the context (so, we can pass the accrual date
into the context.) The non-stored version of those fields have the same
name with the suffix "at_date" (eg.: `qty_invoiced_at_date`.)

To achieve that, we move all the compute code outside of the compute
method to have a method we can call from both fields' compute.
For example, we have now a method named `_prepare_qty_delivered` which
returns the `qty_delivered` value for the SO lines, and both
`_compute_qty_delivered` and `_compute_qty_delivered_at_date` use it.

"at_date" fields
================
Those new computed stored fields use the `accrual_entry_date` context
key to compute their value at this date.
This key was already there in the code and already used by the accrual
wizard but before, it was always an actual date object, now it can be a
string (when pass in the context from the client side) and thus needs to
be parsed.

A field `amount_to_invoice_at_date` was also added, both for PO line and
SO line, and this field is simply the difference between the
delivered/received quantity and the invoiced quantity times the price
unit. It is used by the accrual views (see enterprise PR) and the
accrual wizard.

addons/account/wizard/accrued_orders.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from collections import defaultdict
 from dateutil.relativedelta import relativedelta
 import json
 from odoo import models, fields, api, _, Command
@@ -134,18 +135,24 @@ class AccountAccruedOrdersWizard(models.TransientModel):
 
         self.ensure_one()
         move_lines = []
-        is_purchase = self.env.context.get('active_model') == 'purchase.order'
-        orders = self.env[self.env.context['active_model']].with_company(self.company_id).browse(self.env.context['active_ids'])
+        active_model = self.env.context.get('active_model')
+        if active_model in ['purchase.order.line', 'sale.order.line']:
+            lines = self.env[active_model].with_company(self.company_id).browse(self.env.context['active_ids'])
+            orders = lines.order_id
+        else:
+            orders = self.env[active_model].with_company(self.company_id).browse(self.env.context['active_ids'])
+            lines = orders.order_line.filtered(lambda x: x.product_id)
+        is_purchase = orders._name == 'purchase.order'
 
         if orders.filtered(lambda o: o.company_id != self.company_id):
             raise UserError(_('Entries can only be created for a single company at a time.'))
         if orders.currency_id and len(orders.currency_id) > 1:
             raise UserError(_('Cannot create an accrual entry with orders in different currencies.'))
         orders_with_entries = []
-        fnames = []
         total_balance = 0.0
-        for order in orders:
-            product_lines = order.order_line.filtered(lambda x: x.product_id)
+        amounts_by_perpetual_account = defaultdict(float)
+
+        for order, product_lines in lines.grouped('order_id').items():
             if len(orders) == 1 and product_lines and self.amount and order.order_line:
                 total_balance = self.amount
                 order_line = product_lines[0]
@@ -154,70 +161,64 @@ class AccountAccruedOrdersWizard(models.TransientModel):
                 values = _get_aml_vals(order, self.amount, 0, account.id, label=_('Manual entry'), analytic_distribution=distribution)
                 move_lines.append(Command.create(values))
             else:
-                # create a virtual order that will allow to recompute the qty delivered/received (and dependancies)
-                # without actually writing anything on the real record (field is computed and stored)
-                o = order.new(origin=order)
-                if is_purchase:
-                    o.order_line.with_context(accrual_entry_date=self.date)._compute_qty_received()
-                    o.order_line.with_context(accrual_entry_date=self.date)._compute_qty_invoiced()
-                else:
-                    o.order_line.with_context(accrual_entry_date=self.date)._compute_qty_delivered()
-                    o.order_line.with_context(accrual_entry_date=self.date)._compute_qty_invoiced()
-                    o.order_line.with_context(accrual_entry_date=self.date)._compute_untaxed_amount_invoiced()
-                    o.order_line.with_context(accrual_entry_date=self.date)._compute_qty_to_invoice()
-                lines = o.order_line.filtered(
+                accrual_entry_date = self.env.context.get('accrual_entry_date', self.date)
+                order_lines = lines.with_context(accrual_entry_date=accrual_entry_date).filtered(
                     # We only want non-comment lines (no sections, notes, ...) and include all lines
                     # for purchase orders but exclude downpayment lines for sales orders.
                     lambda l: not l.display_type and not l.is_downpayment and
+                    l.id in order.order_line.ids and
                     fields.Float.compare(
-                        l.qty_to_invoice,
+                        l.amount_to_invoice_at_date,
                         0,
                         precision_rounding=l.product_uom_id.rounding,
                     ) != 0
                 )
-                for order_line in lines:
+                for order_line in order_lines:
+                    product = order_line.product_id
                     if is_purchase:
-                        account = self._get_computed_account(order, order_line.product_id, is_purchase)
+                        expense_account, stock_variation_account = self._get_product_expense_and_stock_var_accounts(product)
+                        account = stock_variation_account if stock_variation_account else self._get_computed_account(order, order_line.product_id, is_purchase)
                         if any(tax.price_include for tax in order_line.tax_ids):
                             # As included taxes are not taken into account in the price_unit, we need to compute the price_subtotal
+                            qty_to_invoice = order_line.qty_received_at_date - order_line.qty_invoiced_at_date
                             price_subtotal = order_line.tax_ids.compute_all(
                                 order_line.price_unit,
                                 currency=order_line.order_id.currency_id,
-                                quantity=order_line.qty_to_invoice,
+                                quantity=qty_to_invoice,
                                 product=order_line.product_id,
                                 partner=order_line.order_id.partner_id)['total_excluded']
                         else:
-                            price_subtotal = order_line.qty_to_invoice * order_line.price_unit
+                            price_subtotal = order_line.amount_to_invoice_at_date
                         amount_currency = order_line.currency_id.round(price_subtotal)
                         amount = order.currency_id._convert(amount_currency, self.company_id.currency_id, self.company_id)
-                        fnames = ['qty_to_invoice', 'qty_received', 'qty_invoiced', 'invoice_lines']
                         label = _(
                             '%(order)s - %(order_line)s; %(quantity_billed)s Billed, %(quantity_received)s Received at %(unit_price)s each',
                             order=order.name,
                             order_line=_ellipsis(order_line.name, 20),
-                            quantity_billed=order_line.qty_invoiced,
-                            quantity_received=order_line.qty_received,
+                            quantity_billed=order_line.qty_invoiced_at_date,
+                            quantity_received=order_line.qty_received_at_date,
                             unit_price=formatLang(self.env, order_line.price_unit, currency_obj=order.currency_id),
                         )
                     else:
-                        account = self._get_computed_account(order, order_line.product_id, is_purchase)
-                        amount_currency = order_line.untaxed_amount_to_invoice
+                        expense_account, stock_variation_account = self._get_product_expense_and_stock_var_accounts(product)
+                        account = self._get_computed_account(order, product, is_purchase)
+                        amount_currency = order_line.amount_to_invoice_at_date
                         amount = order.currency_id._convert(amount_currency, self.company_id.currency_id, self.company_id)
-                        fnames = ['qty_to_invoice', 'untaxed_amount_to_invoice', 'qty_invoiced', 'qty_delivered', 'invoice_lines']
                         label = _(
                             '%(order)s - %(order_line)s; %(quantity_invoiced)s Invoiced, %(quantity_delivered)s Delivered at %(unit_price)s each',
                             order=order.name,
                             order_line=_ellipsis(order_line.name, 20),
-                            quantity_invoiced=order_line.qty_invoiced,
-                            quantity_delivered=order_line.qty_delivered,
+                            quantity_invoiced=order_line.qty_invoiced_at_date,
+                            quantity_delivered=order_line.qty_delivered_at_date,
                             unit_price=formatLang(self.env, order_line.price_unit, currency_obj=order.currency_id),
                         )
+                        if expense_account and stock_variation_account:
+                            label += " (*)"
+                            amounts_by_perpetual_account[expense_account, stock_variation_account] += amount
                     distribution = order_line.analytic_distribution if order_line.analytic_distribution else {}
                     values = _get_aml_vals(order, amount, amount_currency, account.id, label=label, analytic_distribution=distribution)
                     move_lines.append(Command.create(values))
                     total_balance += amount
-                # must invalidate cache or o can mess when _create_invoices().action_post() of original order after this
-                order.order_line.invalidate_model(fnames)
 
         if not self.company_id.currency_id.is_zero(total_balance):
             # globalized counterpart for the whole orders selection
@@ -230,6 +231,18 @@ class AccountAccruedOrdersWizard(models.TransientModel):
                 for account_id, distribution in line.analytic_distribution.items():
                     analytic_distribution.update({account_id : analytic_distribution.get(account_id, 0) + distribution*ratio})
             values = _get_aml_vals(orders, -total_balance, 0.0, self.account_id.id, label=_('Accrued total'), analytic_distribution=analytic_distribution)
+            move_lines.append(Command.create(values))
+
+        for (expense_account, stock_variation_account), amount in amounts_by_perpetual_account.items():
+            if amount == 0:
+                continue
+            if amount > 0:
+                label = _('(*) Goods Delivered not Invoiced (perpetual valuation)')
+            else:
+                label = _('(*) Goods Invoiced not Delivered (perpetual valuation)')
+            values = _get_aml_vals(orders, amount, 0.0, stock_variation_account.id, label=label)
+            move_lines.append(Command.create(values))
+            values = _get_aml_vals(orders, -amount, 0.0, expense_account.id, label=label)
             move_lines.append(Command.create(values))
 
         move_type = _('Expense') if is_purchase else _('Revenue')
@@ -276,3 +289,7 @@ class AccountAccruedOrdersWizard(models.TransientModel):
             'view_mode': 'list,form',
             'domain': [('id', 'in', (move | reverse_move).ids)],
         }
+
+    @api.model
+    def _get_product_expense_and_stock_var_accounts(self, product):
+        return (False, False)

addons/purchase/models/purchase_order_line.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import datetime, time
 from dateutil.relativedelta import relativedelta
 from pytz import UTC
@@ -60,6 +61,20 @@ class PurchaseOrderLine(models.Model):
     qty_received_manual = fields.Float("Manual Received Qty", digits='Product Unit', copy=False)
     qty_to_invoice = fields.Float(compute='_compute_qty_invoiced', string='To Invoice Quantity', store=True, readonly=True,
                                   digits='Product Unit')
+
+    # Same than `qty_received` and `qty_to_invoice` but non-stored and depending of the context.
+    qty_received_at_date = fields.Float(
+        string="Received",
+        compute='_compute_qty_received_at_date',
+        digits='Product Unit'
+    )
+    qty_invoiced_at_date = fields.Float(
+        string="Billed",
+        compute='_compute_qty_invoiced_at_date',
+        digits='Product Unit'
+    )
+
+    amount_to_invoice_at_date = fields.Float(string='Amount', compute='_compute_amount_to_invoice_at_date')
 
     partner_id = fields.Many2one('res.partner', related='order_id.partner_id', string='Partner', readonly=True, store=True, index='btree_not_null')
     currency_id = fields.Many2one(related='order_id.currency_id', string='Currency')
@@ -137,16 +152,10 @@ class PurchaseOrderLine(models.Model):
 
     @api.depends('invoice_lines.move_id.state', 'invoice_lines.quantity', 'qty_received', 'product_uom_qty', 'order_id.state')
     def _compute_qty_invoiced(self):
+        invoiced_quantities = self._prepare_qty_invoiced()
         for line in self:
-            # compute qty_invoiced
-            qty = 0.0
-            for inv_line in line._get_invoice_lines():
-                if inv_line.move_id.state not in ['cancel'] or inv_line.move_id.payment_state == 'invoicing_legacy':
-                    if inv_line.move_id.move_type == 'in_invoice':
-                        qty += inv_line.product_uom_id._compute_quantity(inv_line.quantity, line.product_uom_id)
-                    elif inv_line.move_id.move_type == 'in_refund':
-                        qty -= inv_line.product_uom_id._compute_quantity(inv_line.quantity, line.product_uom_id)
-            line.qty_invoiced = qty
+            if not line.qty_invoiced or line in invoiced_quantities:
+                line.qty_invoiced = invoiced_quantities[line]
 
             # compute qty_to_invoice
             if line.order_id.state == 'purchase':
@@ -157,11 +166,35 @@ class PurchaseOrderLine(models.Model):
             else:
                 line.qty_to_invoice = 0
 
+    @api.depends('qty_invoiced')
+    @api.depends_context('accrual_entry_date')
+    def _compute_qty_invoiced_at_date(self):
+        if not self._date_in_the_past():
+            for line in self:
+                line.qty_invoiced_at_date = line.qty_invoiced
+            return
+        invoiced_quantities = self._prepare_qty_invoiced()
+        for line in self:
+            line.qty_invoiced_at_date = invoiced_quantities[line]
+
+    def _prepare_qty_invoiced(self):
+        # Compute qty_invoiced
+        invoiced_qties = defaultdict(float)
+        for line in self:
+            for inv_line in line._get_invoice_lines():
+                if inv_line.move_id.state not in ['cancel'] or inv_line.move_id.payment_state == 'invoicing_legacy':
+                    if inv_line.move_id.move_type == 'in_invoice':
+                        invoiced_qties[line] += inv_line.product_uom_id._compute_quantity(inv_line.quantity, line.product_uom_id)
+                    elif inv_line.move_id.move_type == 'in_refund':
+                        invoiced_qties[line] -= inv_line.product_uom_id._compute_quantity(inv_line.quantity, line.product_uom_id)
+        return invoiced_qties
+
     def _get_invoice_lines(self):
         self.ensure_one()
         if self.env.context.get('accrual_entry_date'):
+            accrual_date = fields.Date.from_string(self.env.context['accrual_entry_date'])
             return self.invoice_lines.filtered(
-                lambda l: l.move_id.invoice_date and l.move_id.invoice_date <= self.env.context['accrual_entry_date']
+                lambda l: l.move_id.invoice_date and l.move_id.invoice_date <= accrual_date
             )
         else:
             return self.invoice_lines
@@ -176,11 +209,30 @@ class PurchaseOrderLine(models.Model):
 
     @api.depends('qty_received_method', 'qty_received_manual')
     def _compute_qty_received(self):
+        received_qties = self._prepare_qty_received()
+        for line in self:
+            if not line.qty_received or line in received_qties:
+                line.qty_received = received_qties[line]
+
+    @api.depends('qty_received')
+    @api.depends_context('accrual_entry_date')
+    def _compute_qty_received_at_date(self):
+        if not self._date_in_the_past():
+            for line in self:
+                line.qty_received_at_date = line.qty_received
+            return
+        received_quantities = self._prepare_qty_received()
+        for line in self:
+            line.qty_received_at_date = received_quantities[line]
+
+    def _prepare_qty_received(self):
+        received_qties = defaultdict(float)
         for line in self:
             if line.qty_received_method == 'manual':
-                line.qty_received = line.qty_received_manual or 0.0
+                received_qties[line] = line.qty_received_manual or 0.0
             else:
-                line.qty_received = 0.0
+                received_qties[line] = 0.0
+        return received_qties
 
     @api.onchange('qty_received')
     def _inverse_qty_received(self):
@@ -208,6 +260,12 @@ class PurchaseOrderLine(models.Model):
                 line.selected_seller_id = seller.id if seller else False
             else:
                 line.selected_seller_id = False
+
+    @api.depends('price_unit', 'qty_invoiced_at_date', 'qty_received_at_date')
+    @api.depends_context('accrual_entry_date')
+    def _compute_amount_to_invoice_at_date(self):
+        for line in self:
+            line.amount_to_invoice_at_date = (line.qty_received_at_date - line.qty_invoiced_at_date) * line.price_unit
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -590,6 +648,13 @@ class PurchaseOrderLine(models.Model):
         to order user's time zone, convert to UTC time.
         """
         return self.order_id.get_order_timezone().localize(datetime.combine(date, time(12))).astimezone(UTC).replace(tzinfo=None)
+
+    @api.model
+    def _date_in_the_past(self):
+        if not 'accrual_entry_date' in self.env.context:
+            return False
+        accrual_date = fields.Date.from_string(self.env.context['accrual_entry_date'])
+        return accrual_date < fields.Date.today()
 
     def _update_date_planned(self, updated_date):
         self.date_planned = updated_date

addons/purchase_stock/models/purchase_order_line.py
@@ -44,13 +44,17 @@ class PurchaseOrderLine(models.Model):
         self.ensure_one()
         moves = self.move_ids.filtered(lambda m: m.product_id == self.product_id)
         if self.env.context.get('accrual_entry_date'):
-            moves = moves.filtered(lambda r: fields.Date.context_today(r, r.date) <= self.env.context['accrual_entry_date'])
+            accrual_date = fields.Date.from_string(self.env.context['accrual_entry_date'])
+            moves = moves.filtered(lambda r: fields.Date.context_today(r, r.date) <= accrual_date)
         return moves
 
     @api.depends('move_ids.state', 'move_ids.product_uom', 'move_ids.quantity')
     def _compute_qty_received(self):
+        super()._compute_qty_received()
+
+    def _prepare_qty_received(self):
         from_stock_lines = self.filtered(lambda order_line: order_line.qty_received_method == 'stock_moves')
-        super(PurchaseOrderLine, self - from_stock_lines)._compute_qty_received()
+        received_qties = super(PurchaseOrderLine, self - from_stock_lines)._prepare_qty_received()
         for line in self:
             if line.qty_received_method == 'stock_moves':
                 total = 0.0
@@ -72,7 +76,8 @@ class PurchaseOrderLine(models.Model):
                         else:
                             total += move.product_uom._compute_quantity(move.quantity, line.product_uom_id, rounding_method='HALF-UP')
                 line._track_qty_received(total)
-                line.qty_received = total
+                received_qties[line] = total
+        return received_qties
 
     @api.depends('product_uom_qty', 'date_planned')
     def _compute_forecasted_issue(self):

addons/sale_stock/wizard/accrued_orders.py
@@ -1,0 +1,17 @@
+from odoo import models, api
+
+
+class AccountAccruedOrdersWizard(models.TransientModel):
+    _inherit = 'account.accrued.orders.wizard'
+
+    @api.model
+    def _get_product_expense_and_stock_var_accounts(self, product):
+        self.ensure_one()
+        res = super()._get_product_expense_and_stock_var_accounts(product)
+        if product.is_storable and product.valuation == 'real_time':
+            product_accounts = product._get_product_accounts()
+            expense_account = product_accounts.get('expense')
+            stock_variation_account = product_accounts.get('stock_variation')
+            if expense_account and stock_variation_account:
+                return (expense_account, stock_variation_account)
+        return res

LAIDOUDI33/LAID_ODOO

commit 7866691e2f4d5449e64a1b1e58d273535b43e5bc

[FIX] account,purchase: accrued line for ordered quantities products
Version:
--------
- 19.0+

Steps to reproduce:
-------------------
- Install `stock`, `purchase`, and `accountant`
- Create a storable product with:
  - Tracking Inventory enabled
  - Control Policy set to **Ordered Quantities**
- Create and confirm a Purchase Order with some unit price
- Do not receive or invoice the order
- From the Purchase Order gear menu, click **Accrued Expense Entry**

Issue:
------
The Accrued Expense Entry wizard opens, but no accounting lines are
generated.

For products invoiced on **Ordered Quantities**, the ordered quantity
should already be accrued even though nothing has been received.

Cause:
------
This regression was introduced by:

https://github.com/odoo-dev/odoo/commit/81f25bc57b8433a65bf33950c64dc7582240a229

Previously, the accrual wizard relied on the stored
`qty_to_invoice` field, whose computation already respected the
product's Control Policy.

For products invoiced on **Ordered Quantities**,
`_compute_qty_invoiced()` computes the quantity to invoice from the
ordered quantity:

https://github.com/odoo/odoo/blob/810a02a577c2811dc5c12f0abf45eebb9cf96d00/addons/purchase/models/purchase_order_line.py#L147-L152

The refactoring replaced this logic with the new
`amount_to_invoice_at_date` field, which always computes the invoicable
quantity as:

qty_received_at_date - qty_invoiced_at_date
https://github.com/odoo/odoo/blob/65dbcabcd243abf24d6d3c3788d2caff66485790/addons/purchase/models/purchase_order_line.py#L282-L285

This formula ignores the product's Control Policy.

For products invoiced on Ordered Quantities, before any receipt:

qty_received_at_date = 0
qty_invoiced_at_date = 0

therefore:

amount_to_invoice_at_date = 0

The Accrued Expense wizard filters out lines whose
amount_to_invoice_at_date is zero:

https://github.com/odoo/odoo/blob/65dbcabcd243abf24d6d3c3788d2caff66485790/addons/account/wizard/accrued_orders.py#L168-L178

addons/account/wizard/accrued_orders.py
@@ -196,7 +196,7 @@ class AccountAccruedOrdersWizard(models.TransientModel):
                         account = stock_variation_account if stock_variation_account else self._get_computed_account(order, order_line.product_id, is_purchase)
                         if any(tax.price_include for tax in order_line.tax_ids):
                             # As included taxes are not taken into account in the price_unit, we need to compute the price_subtotal
-                            qty_to_invoice = order_line.qty_received_at_date - order_line.qty_invoiced_at_date
+                            qty_to_invoice = order_line._get_qty_to_invoice_at_date()
                             price_subtotal = order_line.tax_ids.compute_all(
                                 order_line._get_gross_price_unit(),
                                 currency=order_line.order_id.currency_id,
@@ -204,7 +204,7 @@ class AccountAccruedOrdersWizard(models.TransientModel):
                                 product=order_line.product_id,
                                 partner=order_line.order_id.partner_id)['total_excluded']
                         else:
-                            price_subtotal = (order_line.qty_received_at_date - order_line.qty_invoiced_at_date) * price_unit
+                            price_subtotal = order_line._get_qty_to_invoice_at_date() * price_unit
                         amount_currency = order_line.currency_id.round(price_subtotal)
                         amount = order.currency_id._convert(amount_currency, self.company_id.currency_id, self.company_id)
                         label = _(

addons/purchase/models/purchase_order_line.py
@@ -277,12 +277,20 @@ class PurchaseOrderLine(models.Model):
             else:
                 line.selected_seller_id = False
 
-    @api.depends('price_unit', 'qty_invoiced_at_date', 'qty_received_at_date')
+    @api.depends('price_unit', 'qty_invoiced_at_date', 'qty_received_at_date', 'product_qty')
     @api.depends_context('accrual_entry_date')
     def _compute_amount_to_invoice_at_date(self):
         for line in self:
-            qty_to_invoice = line.product_uom_id._compute_quantity(line.qty_received_at_date - line.qty_invoiced_at_date, line.product_id.uom_id)
+            quantity_to_invoice = line._get_qty_to_invoice_at_date()
+            qty_to_invoice = line.product_uom_id._compute_quantity(quantity_to_invoice, line.product_id.uom_id)
             line.amount_to_invoice_at_date = qty_to_invoice * line._get_gross_price_unit()
+
+    def _get_qty_to_invoice_at_date(self):
+        """Return the quantity to invoice at the accrual date, respecting the product's purchase method."""
+        self.ensure_one()
+        if self.product_id.purchase_method == 'purchase':
+            return self.product_qty - self.qty_invoiced_at_date
+        return self.qty_received_at_date - self.qty_invoiced_at_date
 
     @api.model_create_multi
     def create(self, vals_list):

addons/purchase_stock/tests/test_purchase_stock_accrued_entries.py
@@ -314,3 +314,26 @@ class TestAccruedPurchaseStock(AccountTestInvoicingCommon):
             {'account_id': stock_price_diff_acc_id.id, 'debit': 300, 'credit': 0},
             {'account_id': account_stock_variation.id, 'debit': 0, 'credit': 300},
         ])
+
+    def test_purchase_stock_accruals_ordered_quantities_no_receipt(self):
+        """Test that accrued expense entries are created from the ordered quantity for
+        a storable product with ordered-quantity control."""
+        self.purchase_order.order_line.product_id.update({
+            'is_storable': True,
+            'purchase_method': 'purchase',
+        })
+        wizard = self.env['account.accrued.orders.wizard'].with_context({
+            'active_model': 'purchase.order',
+            'active_ids': self.purchase_order.ids,
+        }).create({
+            'account_id': self.account_expense.id,
+            'date': fields.Date.context_today(self),
+        })
+        self.assertRecordValues(self.env['account.move'].search(wizard.create_entries()['domain']).line_ids, [
+            # reverse move lines
+            {'account_id': self.account_expense.id, 'debit': 0, 'credit': 300},
+            {'account_id': wizard.account_id.id, 'debit': 300, 'credit': 0},
+            # move lines
+            {'account_id': self.account_expense.id, 'debit': 300, 'credit': 0},
+            {'account_id': wizard.account_id.id, 'debit': 0, 'credit': 300},
+        ])