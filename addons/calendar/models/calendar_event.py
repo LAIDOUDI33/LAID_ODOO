odoo/odoo

[IMP] calendar: display a mail composer when archiving
This commit adapts the code to display a mail composer when archiving an
event. The logics to show the composer and to send the cancellation emails are
the same at the events' deletion. To avoid creating a new wizard to generate
the mail and a new method to determine if the composer must be displayed, the
CalendarEvent.action_unlink is reused and accepts a requested_action
parameter, which determine the final action performed on the record depending
on whether the user wishes to delete it or archive it. This avoids adding new
codes and makes the code easier to maintain.

Enterprise PR: https://github.com/odoo/enterprise/pull/110517
Upgrade PR: https://github.com/odoo/upgrade/pull/10271

Task-5914191

addons/calendar/models/calendar_event.py
@@ -1080,7 +1080,7 @@ class CalendarEvent(models.Model):
         self.env['calendar.alarm_manager']._notify_next_alarm(partner_ids)
         return result
 
-    def _unlink_or_archive(self, recurrence_choice=None):
+    def _action_unlink(self, recurrence_choice=None):
         if len(self) == 1:
             if self.user_id._has_any_active_synchronization():
                 return self.action_mass_archive(recurrence_choice)
@@ -1124,14 +1124,26 @@ class CalendarEvent(models.Model):
             },
         }
 
-    def action_unlink(self, attendee_id=None, next_action=None, recurrence_choice=None):
-        """
-        Delete the event(s) after displaying a delete wizard if necessary.
+    def action_open_delete_wizard(self, attendee_id=None, next_action=None, recurrence_choice=None, send_email=True):
+        return self.action_open_cancel_wizard(
+            'delete',
+            attendee_id=attendee_id,
+            next_action=next_action,
+            recurrence_choice=recurrence_choice,
+            send_email=send_email
+        )
 
+    def action_open_cancel_wizard(self, requested_action=None, attendee_id=None, next_action=None, recurrence_choice=None, send_email=True):
+        """
+        If needed, it displays a modal to edit the cancellation template email and send it to the attendees otherwise it preforms directly
+        the action requested from the interface as deletion.
+
+        :param requested_action: The action requested from the interface triggering the method and can be "delete" or "cancel"
         :param attendee_id: The ID of the attendee for the event
-        :param next_action: The action to perform once the events are deleted
-        :param recurrence_choice: The value specifying which events of a recurrence must be deleted
-        :return: Action to delete the event(s)
+        :param next_action: The action to perform once the events are deleted or cancelled
+        :param recurrence_choice: The value specifying which events of a recurrence must be deleted or cancelled
+        :param send_email: If false, the method performs directly the action requested from the interface and do not offer to send cancellation emails
+        :return: Action to delete or cancel the event(s)
         """
         if not next_action:
             next_action = {'type': 'ir.actions.client', 'tag': 'soft_reload'}
@@ -1141,52 +1153,59 @@ class CalendarEvent(models.Model):
 
         if (
             self.user_id._has_any_active_synchronization()
-            and not (len(self.ids) == 1 and self.recurrency and not recurrence_choice)  # To display the "Delete Recurring Events" form for recurring events.
+            and not (len(self.ids) == 1 and self.recurrency and not recurrence_choice)  # To display the "Select Recurring Events" form for recurring events.
+            or not send_email
         ):
-            self._unlink_or_archive(recurrence_choice)
+            if requested_action == "delete":
+                self._action_unlink(recurrence_choice)
+            elif requested_action == "cancel":
+                self.with_context(block_automatic_cancellation_email=True).write({'active': False})
             return next_action
 
-        action_unlink = {
+        action_open_cancel_wizard = {
             'type': 'ir.actions.act_window',
             'views': [(False, 'form')],
             'target': 'new',
         }
         if len(self) > 1:
-            action_unlink.update({
-                'name': _('Delete Events'),
-                'res_model': 'calendar.event.multi.delete.wizard',
+            action_open_cancel_wizard.update({
+                'name': _('Notify attendees'),
+                'res_model': 'calendar.event.multi.cancel.wizard',
                 'context': {
                     'active_ids': self.ids,
                     'active_model': 'calendar.event',
+                    'default_requested_action': requested_action,
                     'dialog_size': 'small',
                     'next_action': next_action,
                 },
             })
         else:
-            if self.recurrency and not recurrence_choice:
-                action_unlink.update({
-                    'name': _('Delete Recurring Event'),
-                    'res_model': 'calendar.event.delete.wizard',
+            if self.recurrency and not recurrence_choice and requested_action:
+                action_open_cancel_wizard.update({
+                    'name': _('Select Recurring Event'),
+                    'res_model': 'calendar.event.cancel.wizard',
                     'context': {
                         'default_attendee_id': attendee_id,
                         'default_calendar_event_id': self.id,
-                        'form_view_ref': 'calendar.recurring_calendar_event_delete_wizard_view_form',
+                        'default_requested_action': requested_action,
+                        'form_view_ref': 'calendar.recurring_calendar_event_cancel_wizard_view_form',
                         'next_action': next_action,
                     },
                 })
             else:
-                action_unlink.update({
-                    'name': _('Delete Event'),
-                    'res_model': 'calendar.event.delete.wizard',
+                action_open_cancel_wizard.update({
+                    'name': _('Notify attendees'),
+                    'res_model': 'calendar.event.cancel.wizard',
                     'context': {
                         'default_attendee_id': attendee_id,
                         'default_calendar_event_id': self.id,
-                        'default_delete': recurrence_choice,
-                        'form_view_ref': 'calendar.calendar_event_delete_wizard_view_form',
+                        'default_recurrence_choice': recurrence_choice,
+                        'default_requested_action': requested_action,
+                        'form_view_ref': 'calendar.calendar_event_cancel_wizard_view_form',
                         'next_action': next_action,
                     },
                 })
-        return action_unlink
+        return action_open_cancel_wizard
 
     def _mail_get_operation_for_mail_message_operation(self, message_operation):
         # reading messages on private events requires write access, not just read access

addons/calendar/wizard/calendar_event_cancel_wizard.py
@@ -3,27 +3,33 @@
 from odoo import api, fields, models
 
 
-class CalendarEventDeleteWizard(models.TransientModel):
-    _name = 'calendar.event.delete.wizard'
+class CalendarEventCancelWizard(models.TransientModel):
+    _name = 'calendar.event.cancel.wizard'
     _inherit = ['mail.composer.mixin']
-    _description = 'Calendar Event Delete Wizard'
+    _description = 'Calendar Event Cancel Wizard'
 
     calendar_event_id = fields.Many2one('calendar.event', 'Calendar Event')
-    delete = fields.Selection([('self_only', 'Delete this event'), ('future_events', 'Delete this and following events'), ('all_events', 'Delete all the events')], default='self_only')
-    multi_delete_wizard_id = fields.Many2one('calendar.event.multi.delete.wizard', ondelete='cascade')
+    multi_cancel_wizard_id = fields.Many2one('calendar.event.multi.cancel.wizard', ondelete='cascade')
     recipient_ids = fields.Many2many(
         'res.partner',
         string="Recipients",
         compute='_compute_recipient_ids',
         readonly=False,
     )
+    recurrence_choice = fields.Selection([('self_only', 'This event'), ('future_events', 'This and following events'), ('all_events', 'All the events')], default='self_only')
+    requested_action = fields.Selection([('cancel', 'cancel'), ('delete', 'delete')])
     template_id = fields.Many2one('mail.template', default=lambda self: self.env.ref('calendar.calendar_template_delete_event', raise_if_not_found=False))
 
-    def action_proceed_delete_choice(self):
+    def action_proceed_recurrence_choice(self):
         # Return if there are multiple attendees or if the organizer's partner_id differs
+        cancellation_parameters = {
+            'attendee_id': self.calendar_event_id.partner_id.id,
+            'next_action': self.env.context.get('next_action'),
+            'requested_action': self.requested_action,
+        }
         if self.calendar_event_id.attendees_count != 1 or self.calendar_event_id.user_id.partner_id != self.calendar_event_id.partner_ids:
-            return self.calendar_event_id.action_unlink(self.calendar_event_id.partner_id.id, self.env.context.get('next_action'), self.delete)
-        return self.action_delete()
+            return self.calendar_event_id.action_open_cancel_wizard(recurrence_choice=self.recurrence_choice, **cancellation_parameters)
+        return self._get_calendar_events().action_open_cancel_wizard(send_email=False, **cancellation_parameters)
 
     @api.depends('calendar_event_id')
     def _compute_recipient_ids(self):
@@ -53,6 +59,21 @@ class CalendarEventDeleteWizard(models.TransientModel):
                 options={'post_process': True},
             )[wizard.calendar_event_id.id]
 
+    def _get_calendar_events(self):
+        calendar_events = self.env['calendar.event']
+        if self.recurrence_choice in ['self_only', False]:
+            calendar_events += self.calendar_event_id
+        elif self.recurrence_choice == 'future_events':
+            calendar_events += self.calendar_event_id.recurrence_id.calendar_event_ids.filtered(lambda event: event.start >= self.calendar_event_id.start)
+        elif self.recurrence_choice == 'all_events':
+            calendar_events += self.calendar_event_id.recurrence_id.calendar_event_ids
+        return calendar_events
+
+    def action_cancel(self):
+        self.ensure_one()
+        self._get_calendar_events().with_context(block_automatic_cancellation_email=True).write({'active': False})
+        return self.env.context.get('next_action')
+
     def action_delete(self):
         """
         Delete the event based on the specified deletion type.
@@ -60,7 +81,7 @@ class CalendarEventDeleteWizard(models.TransientModel):
         :return: Client action to reload the page.
         """
         self.ensure_one()
-        self.calendar_event_id._unlink_or_archive(self.delete)
+        self.calendar_event_id._action_unlink(self.recurrence_choice)
         return self.env.context.get('next_action')
 
     def _prepare_mail_values(self):
@@ -73,8 +94,17 @@ class CalendarEventDeleteWizard(models.TransientModel):
             'subject': self.subject,
         }
 
+    def action_send_mail(self):
+        self.ensure_one()
+        self.env['mail.mail'].sudo().create([self._prepare_mail_values()])
+
+    def action_send_mail_and_cancel(self):
+        self.ensure_one()
+        self.action_send_mail()
+        return self.action_cancel()
+
     def action_send_mail_and_delete(self):
         """Send the composed email and delete the event based on the specified deletion type."""
         self.ensure_one()
-        self.env['mail.mail'].sudo().create([self._prepare_mail_values()])
+        self.action_send_mail()
         return self.action_delete()

addons/calendar/wizard/calendar_event_multi_cancel_wizard.py
@@ -1,0 +1,41 @@
+from odoo import fields, models
+
+
+class CalendarEventMultiCancelWizard(models.TransientModel):
+    _name = 'calendar.event.multi.cancel.wizard'
+    _description = 'Calendar Event Multi Cancel Wizard'
+
+    def _default_cancel_wizard_ids(self):
+        event_ids = self.env.context.get('active_model') == 'calendar.event' and self.env.context.get('active_ids') or []
+        return [fields.Command.create({'calendar_event_id': event_id}) for event_id in event_ids]
+
+    cancel_wizard_ids = fields.One2many('calendar.event.cancel.wizard', 'multi_cancel_wizard_id', default=_default_cancel_wizard_ids)
+    is_user_admin = fields.Boolean(compute="_compute_is_user_admin")
+    requested_action = fields.Selection([('cancel', 'cancel'), ('delete', 'delete')])
+
+    def _compute_is_user_admin(self):
+        for wizard in self:
+            wizard.is_user_admin = self.env.user._is_admin()
+
+    def action_cancel(self):
+        self.cancel_wizard_ids.calendar_event_id.with_context(block_automatic_cancellation_email=True).write({'active': False})
+        return self.env.context.get('next_action')
+
+    def action_delete(self):
+        self.cancel_wizard_ids.calendar_event_id._action_unlink()
+        return self.env.context.get('next_action')
+
+    def action_send_mails(self):
+        now = fields.Datetime.now()
+        self.env['mail.mail'].sudo().create([
+            wizard._prepare_mail_values() for wizard in self.cancel_wizard_ids
+            if wizard.calendar_event_id.partner_ids != self.env.user.partner_id and wizard.calendar_event_id.start > now
+        ])
+
+    def action_send_mails_and_cancel(self):
+        self.action_send_mails()
+        return self.action_cancel()
+
+    def action_send_mails_and_delete(self):
+        self.action_send_mails()
+        return self.action_delete()

LAIDOUDI33/LAID_ODOO

addons/calendar/models/calendar_event.py
@@ -1080,7 +1080,7 @@ class CalendarEvent(models.Model):
         self.env['calendar.alarm_manager']._notify_next_alarm(partner_ids)
         return result
 
-    def _unlink_or_archive(self, recurrence_choice=None):
+    def _action_unlink(self, recurrence_choice=None):
         if len(self) == 1:
             if self.user_id._has_any_active_synchronization():
                 return self.action_mass_archive(recurrence_choice)
@@ -1124,14 +1124,26 @@ class CalendarEvent(models.Model):
             },
         }
 
-    def action_unlink(self, attendee_id=None, next_action=None, recurrence_choice=None):
-        """
-        Delete the event(s) after displaying a delete wizard if necessary.
+    def action_open_delete_wizard(self, attendee_id=None, next_action=None, recurrence_choice=None, send_email=True):
+        return self.action_open_cancel_wizard(
+            'delete',
+            attendee_id=attendee_id,
+            next_action=next_action,
+            recurrence_choice=recurrence_choice,
+            send_email=send_email
+        )
 
+    def action_open_cancel_wizard(self, requested_action=None, attendee_id=None, next_action=None, recurrence_choice=None, send_email=True):
+        """
+        If needed, it displays a modal to edit the cancellation template email and send it to the attendees otherwise it preforms directly
+        the action requested from the interface as deletion.
+
+        :param requested_action: The action requested from the interface triggering the method and can be "delete" or "cancel"
         :param attendee_id: The ID of the attendee for the event
-        :param next_action: The action to perform once the events are deleted
-        :param recurrence_choice: The value specifying which events of a recurrence must be deleted
-        :return: Action to delete the event(s)
+        :param next_action: The action to perform once the events are deleted or cancelled
+        :param recurrence_choice: The value specifying which events of a recurrence must be deleted or cancelled
+        :param send_email: If false, the method performs directly the action requested from the interface and do not offer to send cancellation emails
+        :return: Action to delete or cancel the event(s)
         """
         if not next_action:
             next_action = {'type': 'ir.actions.client', 'tag': 'soft_reload'}
@@ -1141,52 +1153,59 @@ class CalendarEvent(models.Model):
 
         if (
             self.user_id._has_any_active_synchronization()
-            and not (len(self.ids) == 1 and self.recurrency and not recurrence_choice)  # To display the "Delete Recurring Events" form for recurring events.
+            and not (len(self.ids) == 1 and self.recurrency and not recurrence_choice)  # To display the "Select Recurring Events" form for recurring events.
+            or not send_email
         ):
-            self._unlink_or_archive(recurrence_choice)
+            if requested_action == "delete":
+                self._action_unlink(recurrence_choice)
+            elif requested_action == "cancel":
+                self.with_context(disable_auto_send_cancellation_emails=True).write({'active': False})
             return next_action
 
-        action_unlink = {
+        action_open_cancel_wizard = {
             'type': 'ir.actions.act_window',
             'views': [(False, 'form')],
             'target': 'new',
         }
         if len(self) > 1:
-            action_unlink.update({
-                'name': _('Delete Events'),
-                'res_model': 'calendar.event.multi.delete.wizard',
+            action_open_cancel_wizard.update({
+                'name': _('Notify attendees'),
+                'res_model': 'calendar.event.multi.cancel.wizard',
                 'context': {
                     'active_ids': self.ids,
                     'active_model': 'calendar.event',
+                    'default_requested_action': requested_action,
                     'dialog_size': 'small',
                     'next_action': next_action,
                 },
             })
         else:
-            if self.recurrency and not recurrence_choice:
-                action_unlink.update({
-                    'name': _('Delete Recurring Event'),
-                    'res_model': 'calendar.event.delete.wizard',
+            if self.recurrency and not recurrence_choice and requested_action:
+                action_open_cancel_wizard.update({
+                    'name': _('Select Recurring Event'),
+                    'res_model': 'calendar.event.cancel.wizard',
                     'context': {
                         'default_attendee_id': attendee_id,
                         'default_calendar_event_id': self.id,
-                        'form_view_ref': 'calendar.recurring_calendar_event_delete_wizard_view_form',
+                        'default_requested_action': requested_action,
+                        'form_view_ref': 'calendar.recurring_calendar_event_cancel_wizard_view_form',
                         'next_action': next_action,
                     },
                 })
             else:
-                action_unlink.update({
-                    'name': _('Delete Event'),
-                    'res_model': 'calendar.event.delete.wizard',
+                action_open_cancel_wizard.update({
+                    'name': _('Notify attendees'),
+                    'res_model': 'calendar.event.cancel.wizard',
                     'context': {
                         'default_attendee_id': attendee_id,
                         'default_calendar_event_id': self.id,
-                        'default_delete': recurrence_choice,
-                        'form_view_ref': 'calendar.calendar_event_delete_wizard_view_form',
+                        'default_recurrence_choice': recurrence_choice,
+                        'default_requested_action': requested_action,
+                        'form_view_ref': 'calendar.calendar_event_cancel_wizard_view_form',
                         'next_action': next_action,
                     },
                 })
-        return action_unlink
+        return action_open_cancel_wizard
 
     def _mail_get_operation_for_mail_message_operation(self, message_operation):
         # reading messages on private events requires write access, not just read access

addons/calendar/wizard/calendar_event_cancel_wizard.py
@@ -3,27 +3,33 @@
 from odoo import api, fields, models
 
 
-class CalendarEventDeleteWizard(models.TransientModel):
-    _name = 'calendar.event.delete.wizard'
+class CalendarEventCancelWizard(models.TransientModel):
+    _name = 'calendar.event.cancel.wizard'
     _inherit = ['mail.composer.mixin']
-    _description = 'Calendar Event Delete Wizard'
+    _description = 'Calendar Event Cancel Wizard'
 
     calendar_event_id = fields.Many2one('calendar.event', 'Calendar Event')
-    delete = fields.Selection([('self_only', 'Delete this event'), ('future_events', 'Delete this and following events'), ('all_events', 'Delete all the events')], default='self_only')
-    multi_delete_wizard_id = fields.Many2one('calendar.event.multi.delete.wizard', ondelete='cascade')
+    multi_cancel_wizard_id = fields.Many2one('calendar.event.multi.cancel.wizard', ondelete='cascade')
     recipient_ids = fields.Many2many(
         'res.partner',
         string="Recipients",
         compute='_compute_recipient_ids',
         readonly=False,
     )
+    recurrence_choice = fields.Selection([('self_only', 'This event'), ('future_events', 'This and following events'), ('all_events', 'All the events')], default='self_only')
+    requested_action = fields.Selection([('cancel', 'cancel'), ('delete', 'delete')])
     template_id = fields.Many2one('mail.template', default=lambda self: self.env.ref('calendar.calendar_template_delete_event', raise_if_not_found=False))
 
-    def action_proceed_delete_choice(self):
+    def action_proceed_recurrence_choice(self):
         # Return if there are multiple attendees or if the organizer's partner_id differs
+        cancellation_parameters = {
+            'attendee_id': self.calendar_event_id.partner_id.id,
+            'next_action': self.env.context.get('next_action'),
+            'requested_action': self.requested_action,
+        }
         if self.calendar_event_id.attendees_count != 1 or self.calendar_event_id.user_id.partner_id != self.calendar_event_id.partner_ids:
-            return self.calendar_event_id.action_unlink(self.calendar_event_id.partner_id.id, self.env.context.get('next_action'), self.delete)
-        return self.action_delete()
+            return self.calendar_event_id.action_open_cancel_wizard(recurrence_choice=self.recurrence_choice, **cancellation_parameters)
+        return self._get_calendar_events().action_open_cancel_wizard(send_email=False, **cancellation_parameters)
 
     @api.depends('calendar_event_id')
     def _compute_recipient_ids(self):
@@ -53,6 +59,21 @@ class CalendarEventDeleteWizard(models.TransientModel):
                 options={'post_process': True},
             )[wizard.calendar_event_id.id]
 
+    def _get_calendar_events(self):
+        calendar_events = self.env['calendar.event']
+        if self.recurrence_choice in ['self_only', False]:
+            calendar_events += self.calendar_event_id
+        elif self.recurrence_choice == 'future_events':
+            calendar_events += self.calendar_event_id.recurrence_id.calendar_event_ids.filtered(lambda event: event.start >= self.calendar_event_id.start)
+        elif self.recurrence_choice == 'all_events':
+            calendar_events += self.calendar_event_id.recurrence_id.calendar_event_ids
+        return calendar_events
+
+    def action_cancel(self):
+        self.ensure_one()
+        self._get_calendar_events().with_context(disable_auto_send_cancellation_emails=True).write({'active': False})
+        return self.env.context.get('next_action')
+
     def action_delete(self):
         """
         Delete the event based on the specified deletion type.
@@ -60,7 +81,7 @@ class CalendarEventDeleteWizard(models.TransientModel):
         :return: Client action to reload the page.
         """
         self.ensure_one()
-        self.calendar_event_id._unlink_or_archive(self.delete)
+        self.calendar_event_id._action_unlink(self.recurrence_choice)
         return self.env.context.get('next_action')
 
     def _prepare_mail_values(self):
@@ -73,8 +94,17 @@ class CalendarEventDeleteWizard(models.TransientModel):
             'subject': self.subject,
         }
 
+    def action_send_mail(self):
+        self.ensure_one()
+        self.env['mail.mail'].sudo().create([self._prepare_mail_values()])
+
+    def action_send_mail_and_cancel(self):
+        self.ensure_one()
+        self.action_send_mail()
+        return self.action_cancel()
+
     def action_send_mail_and_delete(self):
         """Send the composed email and delete the event based on the specified deletion type."""
         self.ensure_one()
-        self.env['mail.mail'].sudo().create([self._prepare_mail_values()])
+        self.action_send_mail()
         return self.action_delete()

addons/calendar/wizard/calendar_event_multi_cancel_wizard.py
@@ -1,0 +1,41 @@
+from odoo import fields, models
+
+
+class CalendarEventMultiCancelWizard(models.TransientModel):
+    _name = 'calendar.event.multi.cancel.wizard'
+    _description = 'Calendar Event Multi Cancel Wizard'
+
+    def _default_cancel_wizard_ids(self):
+        event_ids = self.env.context.get('active_model') == 'calendar.event' and self.env.context.get('active_ids') or []
+        return [fields.Command.create({'calendar_event_id': event_id}) for event_id in event_ids]
+
+    cancel_wizard_ids = fields.One2many('calendar.event.cancel.wizard', 'multi_cancel_wizard_id', default=_default_cancel_wizard_ids)
+    is_user_admin = fields.Boolean(compute="_compute_is_user_admin")
+    requested_action = fields.Selection([('cancel', 'cancel'), ('delete', 'delete')])
+
+    def _compute_is_user_admin(self):
+        for wizard in self:
+            wizard.is_user_admin = self.env.user._is_admin()
+
+    def action_cancel(self):
+        self.cancel_wizard_ids.calendar_event_id.with_context(disable_auto_send_cancellation_emails=True).write({'active': False})
+        return self.env.context.get('next_action')
+
+    def action_delete(self):
+        self.cancel_wizard_ids.calendar_event_id._action_unlink()
+        return self.env.context.get('next_action')
+
+    def action_send_mails(self):
+        now = fields.Datetime.now()
+        self.env['mail.mail'].sudo().create([
+            wizard._prepare_mail_values() for wizard in self.cancel_wizard_ids
+            if wizard.calendar_event_id.partner_ids != self.env.user.partner_id and wizard.calendar_event_id.start > now
+        ])
+
+    def action_send_mails_and_cancel(self):
+        self.action_send_mails()
+        return self.action_cancel()
+
+    def action_send_mails_and_delete(self):
+        self.action_send_mails()
+        return self.action_delete()