odoo/odoo

[IMP] calendar, *: create the draft state
* = google_calendar, microsoft_calendar, web

This commit creates a draft state for the calendar events. This state aims to
allow users to create events without confirmed them directly and notifying
users. So it allows users to take time to fill the event with correct
information and avoid spamming attendees with updates.

This state is only available at the creation, in the advanced form opened from
the quick create form. Once the draft state is removed, the attendees can be
notified. It cannot be restored once it has been removed to prevent
notification issues. Otherwise, it could, for example, prevent update messages
from being sent or be used as cancellation state which it is not as it does
not send cancellation messages.

The default value of CalendarEvent.is_draft is False as this state slows down
events' process. It is assumed that users generally have all the necessary
information when creating an event.

The draft recurrent events are not allowed in order to keep the management of
the state as easy as possible. The users should not be able to set to draft
an event on their own to avoid notification issues. So the draft field cannot
be present on the calendar event form and should be unset only from a button
in the header of the form. Thus, there is no easy way to unset the draft state
of an event and to propagate this change on the other events of the recurrence.
For this reason, the fields related to the recurrency are hidden on the draft
events' form.

The draft events are not included in the Google and Microsoft calendars'
synchronisation since those Google calendar do not have a similar state.
Otherwise, this last one could send notifications to the attendees, whereas the
event is not confirmed yet.

The draft events are faded in the gantt and calendar views to warn users that
they are not yet confirmed.

Activities can be created for the draft events. "[draft]" appears in their name
to warn the users that the e

addons/calendar/wizard/calendar_attendee_invite_wizard.py
@@ -6,10 +6,19 @@ class CalendarAttendeeInviteWizard(models.TransientModel):
     _description = 'Calendar Attendee Invite Wizard'
 
     calendar_attendee_ids = fields.Many2many('calendar.attendee')
+    is_confirmation_required = fields.Boolean('Is confirmation required?')
 
     def action_close(self):
         return self.env.context.get('next_action')
 
+    def action_confirm(self):
+        self.calendar_attendee_ids.event_id.with_context(block_automatic_invitation_email=True)._action_confirm()
+        return self.action_close()
+
     def action_send(self):
         self.calendar_attendee_ids._send_invitation_emails()
         return self.action_close()
+
+    def action_send_and_confirm(self):
+        self.action_confirm()  # Must be placed before the sending as emails cannot be sent for draft events.
+        return self.action_send()

LAIDOUDI33/LAID_ODOO

addons/calendar/wizard/calendar_attendee_invite_wizard.py
@@ -6,10 +6,19 @@ class CalendarAttendeeInviteWizard(models.TransientModel):
     _description = 'Calendar Attendee Invite Wizard'
 
     calendar_attendee_ids = fields.Many2many('calendar.attendee')
+    is_confirmation_required = fields.Boolean('Is confirmation required?')
 
     def action_close(self):
         return self.env.context.get('next_action')
 
+    def action_confirm(self):
+        self.calendar_attendee_ids.event_id.with_context(disable_auto_send_invitation_emails=True)._action_confirm()
+        return self.action_close()
+
     def action_send(self):
         self.calendar_attendee_ids._send_invitation_emails()
         return self.action_close()
+
+    def action_send_and_confirm(self):
+        self.action_confirm()  # Must be placed before the sending as emails cannot be sent for draft events.
+        return self.action_send()