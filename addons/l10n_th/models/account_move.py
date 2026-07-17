8697a42153c0119b0b14f10105cb0ee93d4a6deb
soza-odoo <soza@odoo.com>
2026-07-17 16:27:19 +0530

    [IMP] l10n_th:  Improve debit/credit note PDF reports
    In this commit:
    - Display Original Amount, Corrected Amount, and Difference Amount in Thailand
      invoice PDF  for credit notes and debit notes.
    - Fix the report header to display 'Debit Note' instead of 'Tax Invoice' when
      printing debit notes.
    
    task-6246063

--- addons/l10n_th/models/account_move.py
+++ addons/l10n_th/models/account_move.py
@@ -9,3 +9,44 @@ class AccountMove(models.Model):
         if self.company_id.account_fiscal_country_id.code == 'TH':
             return 'l10n_th.report_invoice_document'
         return super()._get_name_invoice_report()
+
+    def _l10n_th_get_credit_debit_note_amounts(self):
+        self.ensure_one()
+
+        if original_move := self.reversed_entry_id:
+            original_amount = original_move.amount_untaxed - sum(
+                original_move.reversal_move_ids.filtered(
+                    lambda move: (
+                        move.state == 'posted'
+                        and move.invoice_date <= self.invoice_date
+                        and move != self
+                    )
+                ).mapped('amount_untaxed')
+            )
+            corrected_amount = original_amount - self.amount_untaxed
+
+        elif 'debit_origin_id' in self._fields and (original_move := self.debit_origin_id):
+            original_amount = original_move.amount_untaxed + sum(
+                original_move.debit_note_ids.filtered(
+                    lambda move: (
+                        move.state == 'posted'
+                        and move.invoice_date <= self.invoice_date
+                        and move != self
+                    )
+                ).mapped('amount_untaxed')
+            )
+            corrected_amount = original_amount + self.amount_untaxed
+
+        else:
+            return []
+
+        return [
+            {
+                'label': self.env._("Original Amount"),
+                'amount': original_amount,
+            },
+            {
+                'label': self.env._("Correct Amount"),
+                'amount': corrected_amount,
+            },
+        ]
