62d4bb4d649b40cd5ed6e436468fb634dbad6e78
Dirk Douglas <dido@odoo.com>
2026-07-20 12:34:44 -0400

    [FIX] website: Ensure cached response is flatten()
    **Problem:**
    Normally, the cached response for an HTTP request will be 'flattened', meaning
    the QWEB template is force-rendered and stored as `response.response[0]`.
    
    However, when the cached response is too old, a new cache value is set which is
    not flattened. This is inconsistent with the value normally returned by
    `_get_response_cached()` which will cause a traceback when accessing
    `response.response[0]`.
    
    This issue is rarely reproducible because `flatten()` is usually called on the
    response later (outside `_get_response()`), and because the returned response
    points to the same object as in the cache, the cache gets flattened as well.
    
    **Solution:**
    When the cached response is too old, flatten the new response before caching
    it.
    
    opw-6382359

--- addons/website/models/website_page.py
+++ addons/website/models/website_page.py
@@ -396,6 +396,7 @@ class WebsitePage(models.Model):
             # The cached response is too old and considered out-of-date. Get it
             # from scratch and update the cache accordingly.
             response = self._get_response_raw(request)
+            response.flatten()
             self._get_response_cached.__cache__.add_value(self, request, cache_value=(response, cache_key))
             return response
 
