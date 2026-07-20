9f2a1d9b7979c99d86efb5c9f34140beeab61ac7
abd-msyukyu-odoo <abd@odoo.com>
2026-07-20 09:17:22 +0200

    WIP on images format + background + gradient + svg
    wip on background + gradient thingy
    
    wip in pause, because images are problematic -> handling format-mimetype
    
    [FIX] mass_mailing images fix (image attributes)
    
    changes caused by https://github.com/odoo/odoo/commit/f96e17b2d22b992ad088a1cce9699d3ea3484b9b
    
    replace data-original-id by data-attachment-id
    and data-original-mimetype by data-format-mimetype
    
    better file name for image formatting for emails
    
    [FIX] html_editor: think about background-image a little
    
    start dev on image idea
    
    ensure all base64 elements are handled, even unmarked
    
    fixes
    
    fixes
    
    fixes
    
    handle align-self
    
    wip on trying to download images that are not attachments not data
    
    wip
    
    WIP creating attachment from non-attachment src
    
    pending
    
    working on images -> WIP on create attachment until we can't
    
    codeview bug + operation + handling savependingimages
    
    wip
    
    full support heh
    
    remove handling of base64 for codeview

--- addons/mass_mailing/static/src/fields/html_field/mass_mailing_html_field.js
+++ addons/mass_mailing/static/src/fields/html_field/mass_mailing_html_field.js
@@ -24,6 +24,7 @@ import { resolveRefEl } from "@web/core/utils/ref_utils";
 import { useEmailHtmlConverter } from "@mail/convert_inline/hooks";
 import { fixInvalidHTML } from "@html_editor/utils/sanitize";
 import { useRecordObserver } from "@web/model/relational_model/utils";
+import { Operation } from "@html_builder/core/operation";
 
 export class MassMailingHtmlField extends HtmlField {
     static template = "mass_mailing.HtmlField";
@@ -279,6 +280,7 @@ export class MassMailingHtmlField extends HtmlField {
             record: this.props.record,
             mobileBreakpoint: "md",
             onEditorReady: () => this.commitChanges(),
+            measureReference: this.converter.measureReference,
         };
     }
 
@@ -294,6 +296,7 @@ export class MassMailingHtmlField extends HtmlField {
         return {
             ...config,
             onEditorReady: () => this.commitChanges(),
+            measureReference: this.converter.measureReference,
             Plugins: [
                 ...MAIN_EDITOR_PLUGINS,
                 ...DYNAMIC_FIELD_PLUGINS,
@@ -411,10 +414,36 @@ export class MassMailingHtmlField extends HtmlField {
      * Ensure that every SVG and WEBP images are converted to PNG, and create
      * an attachment for every b64 encoded image, to ensure every image src
      * is not a data url.
+     * Prevent the user from making additional changes during the operation.
      * @override
      */
-    savePendingImages(content) {
-        return this.editor.shared["imageEmailFormat"].sanitizeImages(content);
+    async savePendingImages(content) {
+        const operation = this.editor.shared.operation
+            ? this.editor.shared.operation
+            : new Operation(this.editor.document);
+        let result;
+        await operation.next(
+            async () => {
+                result = await this.editor.shared.emailImageFormat.sanitizeImages(content);
+                const lastChangeIdSnapshot = this.lastChangeId;
+                if (
+                    this.editor.shared.history.commit() &&
+                    this.lastChangeId === lastChangeIdSnapshot + 1
+                ) {
+                    // All pending image changes were done in both the content
+                    // clone and the editable, so if an editor commit was made
+                    // during this operation, it is reasonable to assume that
+                    // the changes it contains were also done in content, which
+                    // will be sent to the server. Therefore this commit
+                    // onChange should not have increased lastChangeId and the
+                    // snapshotted value is restored, allowing the field to
+                    // not be dirty after updateValue.
+                    this.lastChangeId = lastChangeIdSnapshot;
+                }
+            },
+            { canTimeout: false, shouldInterceptClick: true }
+        );
+        return result;
     }
 
     /**
