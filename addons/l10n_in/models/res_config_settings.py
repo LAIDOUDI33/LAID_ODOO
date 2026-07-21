odoo/odoo

[IMP] l10n_in_*: optional HSN for B2C
The GSTIN has updated the rules for B2C HSN reporting. Now it has become
optional for businesses having a turnover of less than 5 Cr. This improvement
aims to integrate this change into our system by providing a boolean in
settings. The user can switch on/off the reporting mechanism using this boolean.

task-6097595

addons/l10n_in/models/res_config_settings.py
@@ -72,6 +72,7 @@ class ResConfigSettings(models.TransientModel):
         readonly=False
     )
     l10n_in_gst_efiling_feature = fields.Boolean(string="GST Reports & E-Filing")
+    l10n_in_is_b2c_hsn_reporting = fields.Boolean(related='company_id.l10n_in_is_b2c_hsn_reporting', readonly=False)
     l10n_in_fetch_vendor_edi_feature = fields.Boolean(string="Fetch Vendor E-Invoiced Document")
     l10n_in_enet_vendor_batch_payment_feature = fields.Boolean(string="ENet Vendor Batch Payment")
 

LAIDOUDI33/LAID_ODOO

addons/l10n_in/models/res_config_settings.py
@@ -72,6 +72,7 @@ class ResConfigSettings(models.TransientModel):
         readonly=False
     )
     l10n_in_gst_efiling_feature = fields.Boolean(string="GST Reports & E-Filing")
+    l10n_in_disable_b2c_hsn_reporting = fields.Boolean(related='company_id.l10n_in_disable_b2c_hsn_reporting', readonly=False)
     l10n_in_fetch_vendor_edi_feature = fields.Boolean(string="Fetch Vendor E-Invoiced Document")
     l10n_in_enet_vendor_batch_payment_feature = fields.Boolean(string="ENet Vendor Batch Payment")
 