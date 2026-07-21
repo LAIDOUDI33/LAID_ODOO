odoo/odoo

[IMP] website, *: dynamic snippet carousels with title on left
[*]=website_sale

Before this commit, when the section title is placed on the
left and the content-width is not max in total four elements are
displayed (either two or one in each row).

This commit ensures that two items are displayed side-by-side in all
dynamic snippet related carousels when the section title is placed on
the left and the content-width is not max (container-fluid).

task-5090468

addons/website/static/tests/builder/website_builder/dynamic_snippet_carousels_title.test.js
@@ -1,0 +1,66 @@
+import { expect, test } from "@odoo/hoot";
+import { animationFrame, click, queryOne } from "@odoo/hoot-dom";
+import { onRpc } from "@web/../tests/web_test_helpers";
+import {
+    defineWebsiteModels,
+    setupWebsiteBuilderWithSnippet,
+} from "@website/../tests/builder/website_helpers";
+import {
+    DEFAULT_NUMBER_OF_ELEMENTS,
+    DEFAULT_NUMBER_OF_ELEMENTS_FOR_TITLE_LEFT,
+} from "@website/utils/constants";
+
+defineWebsiteModels();
+
+async function checkNumberOfElements(actionParam, dataLabel, expectedValue, snippetEl) {
+    await click(
+        `[data-container-title='Dynamic Carousel'] [data-label='${dataLabel}'] button[data-action-param*='${actionParam}']`
+    );
+    await animationFrame();
+    expect(snippetEl.dataset.numberOfElements).toBe(String(expectedValue));
+}
+
+test("dynamic snippet carousels: adjusts numberOfElements based on title position and content width", async () => {
+    onRpc("/website/snippet/options_filters", async () => [
+        {
+            id: 1,
+            name: "Carousel Items",
+            limit: 10,
+            model_name: "base",
+            help: "Show carousel items",
+        },
+    ]);
+
+    onRpc("/website/snippet/filter_templates", async () => [
+        {
+            key: "website.dynamic_filter_template_base_carousel_item",
+            name: "Carousel Item Template",
+            numberOfElements: "4",
+        },
+    ]);
+    const snippetSelector = ":iframe .s_dynamic_snippet_carousel";
+    await setupWebsiteBuilderWithSnippet("s_dynamic_snippet_carousel");
+    expect(`${snippetSelector}.o_dynamic_snippet_carousel`).toHaveCount(1);
+    const snippetEl = queryOne(snippetSelector);
+    snippetEl.dataset.filterId = "1";
+    await click(snippetSelector);
+    await animationFrame();
+    await checkNumberOfElements(
+        "s_dynamic_snippet_title_aside",
+        "Section Title",
+        DEFAULT_NUMBER_OF_ELEMENTS_FOR_TITLE_LEFT,
+        snippetEl
+    );
+    await checkNumberOfElements(
+        "container-fluid",
+        "Content Width",
+        DEFAULT_NUMBER_OF_ELEMENTS,
+        snippetEl
+    );
+    await checkNumberOfElements(
+        "o_container_small",
+        "Content Width",
+        DEFAULT_NUMBER_OF_ELEMENTS_FOR_TITLE_LEFT,
+        snippetEl
+    );
+});

LAIDOUDI33/LAID_ODOO

addons/website/static/tests/builder/website_builder/dynamic_snippet_carousels_title.test.js
@@ -1,0 +1,66 @@
+import { expect, test } from "@odoo/hoot";
+import { animationFrame, click, queryOne } from "@odoo/hoot-dom";
+import { onRpc } from "@web/../tests/web_test_helpers";
+import {
+    defineWebsiteModels,
+    setupWebsiteBuilderWithSnippet,
+} from "@website/../tests/builder/website_helpers";
+import {
+    DYNAMIC_SNIPPET_DEFAULT_ITEMS_PER_ROW,
+    DYNAMIC_SNIPPET_DEFAULT_ITEMS_PER_ROW_LEFT_TITLE,
+} from "@website/utils/dynamic_snippets";
+
+defineWebsiteModels();
+
+async function checkNumberOfElements(actionParam, dataLabel, expectedValue, snippetEl) {
+    await click(
+        `[data-container-title='Dynamic Carousel'] [data-label='${dataLabel}'] button[data-action-param*='${actionParam}']`
+    );
+    await animationFrame();
+    expect(snippetEl.dataset.numberOfElements).toBe(String(expectedValue));
+}
+
+test("dynamic snippet carousels: adjusts numberOfElements based on title position and content width", async () => {
+    onRpc("/website/snippet/options_filters", async () => [
+        {
+            id: 1,
+            name: "Carousel Items",
+            limit: 10,
+            model_name: "base",
+            help: "Show carousel items",
+        },
+    ]);
+
+    onRpc("/website/snippet/filter_templates", async () => [
+        {
+            key: "website.dynamic_filter_template_base_carousel_item",
+            name: "Carousel Item Template",
+            numberOfElements: "4",
+        },
+    ]);
+    const snippetSelector = ":iframe .s_dynamic_snippet_carousel";
+    await setupWebsiteBuilderWithSnippet("s_dynamic_snippet_carousel");
+    expect(`${snippetSelector}.o_dynamic_snippet_carousel`).toHaveCount(1);
+    const snippetEl = queryOne(snippetSelector);
+    snippetEl.dataset.filterId = "1";
+    await click(snippetSelector);
+    await animationFrame();
+    await checkNumberOfElements(
+        "s_dynamic_snippet_title_aside",
+        "Section Title",
+        DYNAMIC_SNIPPET_DEFAULT_ITEMS_PER_ROW_LEFT_TITLE,
+        snippetEl
+    );
+    await checkNumberOfElements(
+        "container-fluid",
+        "Content Width",
+        DYNAMIC_SNIPPET_DEFAULT_ITEMS_PER_ROW,
+        snippetEl
+    );
+    await checkNumberOfElements(
+        "o_container_small",
+        "Content Width",
+        DYNAMIC_SNIPPET_DEFAULT_ITEMS_PER_ROW_LEFT_TITLE,
+        snippetEl
+    );
+});