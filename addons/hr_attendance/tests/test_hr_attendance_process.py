de3b05f20e2bc60a956f5fadf9261183c5e94fce
Tyler Landivar (tylan) <tylan@odoo.com>
2026-07-24 14:56:25 +0000

    [FIX] hr_attendance: can't check into secondary company with systray
    Steps to reproduce:
    - Install employees and attendance app
    - Make sure there are 2 companies
    - Make user's employee record for Company B, but not A
    - Make company A the default company for user
    - Enable "attendances from backend" setting
    -  Click on the attendance dot (systray)
    
    Current Behavior:
    The dot disappears and you can't check in
    
    Expected Behavior:
    You are able to check in
    
    Other bug scenario:
    If you have employee records in both Company A and Company B,
    you can check in. However, you can never check in for Company B
    as the default company is always selected in the server code
    
    opw-6392301
    
    X-original-commit: ab2b04f1b1d1213a7f026ebd3bedc88e7ab65cd0

--- addons/hr_attendance/tests/test_hr_attendance_process.py
+++ addons/hr_attendance/tests/test_hr_attendance_process.py
@@ -5,12 +5,20 @@ from datetime import datetime
 from unittest.mock import patch
 
 from odoo import fields
+<<<<<<< 620c1e6510cee407b9962823a622b8405f7420ea
 from odoo.tests import Form, new_test_user
 from odoo.tests.common import tagged, TransactionCase, freeze_time
+||||||| 31ed6826e4c6cafb05dea0c22733949f31dfae98
+from odoo.tests import new_test_user
+from odoo.tests.common import tagged, TransactionCase, freeze_time
+=======
+from odoo.tests import new_test_user
+from odoo.tests.common import HttpCase, tagged, TransactionCase, freeze_time
+>>>>>>> f771a0f45a384d465249cdf4ee81093890d42725
 
 
 @tagged('attendance_process')
-class TestHrAttendance(TransactionCase):
+class TestHrAttendance(HttpCase, TransactionCase):
     """Test for presence validity"""
 
     @classmethod
@@ -173,3 +181,33 @@ class TestHrAttendance(TransactionCase):
             self.test_employee.with_user(self.hr_user).action_archive()
             self.assertTrue(not self.test_employee.active, "Employee should be archived successfully with sudo()")
             self.assertEqual(test_attendance.check_out, fields.Datetime.now(), "Attendance should be checked out at the time of archiving")
+
+    def test_attendance_multicompany(self):
+        """Test that the attendance is for the currently selected company, not default company of user"""
+
+        first_company = self.user.employee_id.company_id
+        self.assertTrue(first_company)
+
+        other_company = self.env["res.company"].create({
+            "name": "Test"
+        })
+        other_employee = self.env["hr.employee"].create({
+            "name": self.user.name,
+            "company_id": other_company.id,
+            "user_id": self.user.id
+        })
+
+        self.user.password = 'password'
+        self.authenticate(self.user.login, 'password')
+
+        # first case check when no cids sent, second case check when cids included
+        test_cases = [
+            ("", self.user.employee_id.id),
+            (f"{other_company.id}-{first_company.id}", other_employee.id),
+        ]
+
+        for cids, expected_employee_id in test_cases:
+            with self.subTest(expected_employee_id=expected_employee_id):
+                self.opener.cookies.set('cids', cids)
+                employee = self.make_jsonrpc_request("/hr_attendance/systray_check_in_out", {})
+                self.assertEqual(employee["id"], expected_employee_id)
