odoo/odoo

[IMP] mail: ask confirmation before switching calls
Before this commit, when users attempted to join or start a
new call while already participating in another one, the
active call was switched immediately without any
confirmation.

This commit adds a confirmation dialog before switching
calls. The switch is aborted when the user declines the
confirmation and only proceeds when explicitly confirmed.
This helps prevent accidental call switches and unintended
exits from ongoing calls.

task-6230482

addons/mail/static/src/discuss/call/common/call_invitation.js
@@ -37,11 +37,14 @@ export class CallInvitation extends Component {
     }
 
     async joinCall() {
-        this.props.channel.open({ focus: true });
-        await this.rtc.toggleCall(this.props.channel, {
+        const hasJoinedCall = await this.rtc.toggleCall(this.props.channel, {
             audio: this.state.hasMicrophone,
             camera: this.state.hasCamera,
         });
+        if (!hasJoinedCall) {
+            return;
+        }
+        this.props.channel.open({ focus: true });
         if (this.props.channel.default_display_mode === "video_full_screen") {
             await this.rtc.enterFullscreen();
         }

addons/mail/static/tests/discuss/call/call.test.js
@@ -1119,6 +1119,7 @@ test("Shows warning badge on mic/camera on non-granted permission in meeting con
     await rtc.exitFullscreen();
     await click(".o-mail-DiscussSidebarChannel:text('General')");
     await click("[title='Join Call']");
+    await click(".modal-footer button:text('Confirm')");
     await contains("button[title='Turn camera on']");
     await contains("button[title='Turn camera on'].o-tag-DANGER", { count: 0 });
     await contains("button[title='Turn camera on'].o-tag-WARNING_BADGE", { count: 0 });
@@ -1676,4 +1677,22 @@ test("Adjust view: sidebar layout always shows the sidebar, even alone", async (
     await contains(".o-mail-Meeting");
     // Sidebar mode always shows the sidebar column, even with a single participant.
     await contains(".o-discuss-Call-sidebar");
+});
+
+test("confirm before switching calls", async () => {
+    const pyEnv = await startServer();
+    const channelIds = pyEnv["discuss.channel"].create([{ name: "channel" }, { name: "channel2" }]);
+    await start();
+    await openDiscuss(channelIds[0]);
+    await click("[title='Start Call']");
+    await contains(".o-discuss-CallMenu-channelInfo:text('channel')");
+    await click(".o-mail-DiscussSidebarChannel-itemName:text('channel2')");
+    await contains(".o-mail-AutoresizeInput[title='channel2']");
+    await click("[title='Start Call']");
+    await click(".modal-footer button:text('Discard')");
+    await contains(".modal-footer", { count: 0 });
+    await contains(".o-discuss-CallMenu-channelInfo:text('channel')");
+    await click("[title='Start Call']");
+    await click(".modal-footer button:text('Confirm')");
+    await contains(".o-discuss-CallMenu-channelInfo:text('channel2')");
 });

LAIDOUDI33/LAID_ODOO

addons/mail/static/src/discuss/call/common/call_invitation.js
@@ -39,11 +39,14 @@ export class CallInvitation extends Component {
     }
 
     async joinCall() {
-        this.props.channel.open({ focus: true });
-        await this.rtc.toggleCall(this.props.channel, {
+        const hasJoinedCall = await this.rtc.requestToggleCall(this.props.channel, {
             audio: this.state.hasMicrophone,
             camera: this.state.hasCamera,
         });
+        if (!hasJoinedCall) {
+            return;
+        }
+        this.props.channel.open({ focus: true });
         if (this.props.channel.default_display_mode === "video_full_screen") {
             await this.rtc.enterFullscreen();
         }

addons/mail/static/tests/discuss/call/call.test.js
@@ -1289,6 +1289,10 @@ test("Shows warning badge on mic/camera on non-granted permission in meeting con
     await click(".o-mail-MessagingMenu-tab[data-id='channel']");
     await click(".o-mail-NotificationItem:has(:text('General'))");
     await click("[title='Join Call']");
+    await contains(
+        ".modal:has(:text('Switch to the other call? This will disconnect you from your ongoing call.'))"
+    );
+    await click(".modal-footer button:text('Switch')");
     await contains("button[title='Turn camera on']");
     await contains("button[title='Turn camera on'].o-tag-DANGER", { count: 0 });
     await contains("button[title='Turn camera on'].o-tag-WARNING_BADGE", { count: 0 });
@@ -1818,4 +1822,28 @@ test("Adjust view: sidebar layout always shows the sidebar, even alone", async (
     await contains(".o-mail-Meeting");
     // Sidebar mode always shows the sidebar column, even with a single participant.
     await contains(".o-discuss-Call-sidebar");
+});
+
+test("confirm before switching calls", async () => {
+    const pyEnv = await startServer();
+    const channelIds = pyEnv["discuss.channel"].create([{ name: "channel" }, { name: "channel2" }]);
+    await start();
+    await openDiscuss(channelIds[0]);
+    await click("[title='Start Call']");
+    await contains(".o-discuss-CallMenu-channelInfo:text('channel')");
+    await click(".o-mail-NotificationItem-name:text('channel2')");
+    await contains(".o-mail-AutoresizeInput[title='channel2']");
+    await click("[title='Start Call']");
+    await contains(
+        ".modal:has(:text('Switch to the other call? This will disconnect you from your ongoing call.'))"
+    );
+    await click(".modal-footer button:text('Cancel')");
+    await contains(".modal", { count: 0 });
+    await contains(".o-discuss-CallMenu-channelInfo:text('channel')");
+    await click("[title='Start Call']");
+    await contains(
+        ".modal:has(:text('Switch to the other call? This will disconnect you from your ongoing call.'))"
+    );
+    await click(".modal-footer button:text('Switch')");
+    await contains(".o-discuss-CallMenu-channelInfo:text('channel2')");
 });