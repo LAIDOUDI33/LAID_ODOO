odoo/odoo

[IMP] html_editor: show preview on dropdown hover
Purpose:
Displays a preview when hovering over toolbar dropdown items.

task-6106102

addons/html_editor/static/src/core/overlay.js
@@ -52,6 +52,10 @@ export class EditorOverlay extends Component {
             getTarget = this.getSelectionTarget.bind(this);
         }
 
+        let isPreviewActive = false;
+        useExternalListener(this.props.bus, "previewChange", ({ detail }) => {
+            isPreviewActive = detail.isPreviewActive;
+        });
         useExternalListener(this.props.bus, "updatePosition", () => {
             position.unlock();
         });
@@ -60,7 +64,9 @@ export class EditorOverlay extends Component {
 
         if (this.props.positionOptions?.updatePositionOnResize ?? true) {
             const resizeObserver = new ResizeObserver(() => {
-                position.unlock();
+                if (!isPreviewActive) {
+                    position.unlock();
+                }
             });
             useLayoutEffect(
                 (root) => {

LAIDOUDI33/LAID_ODOO

addons/html_editor/static/src/core/overlay.js
@@ -51,13 +51,19 @@ export class EditorOverlay extends Component {
             getTarget = this.getSelectionTarget.bind(this);
         }
 
+        let isPreviewActive = false;
+        useListener(this.props.bus, "previewChange", ({ detail }) => {
+            isPreviewActive = detail.isPreviewActive;
+        });
         useListener(this.props.bus, "updatePosition", () => {
             position.unlock();
         });
 
         if (this.props.positionOptions?.updatePositionOnResize ?? true) {
             const resizeObserver = new ResizeObserver(() => {
-                position.unlock();
+                if (!isPreviewActive) {
+                    position.unlock();
+                }
             });
             useLayoutEffect(
                 (root) => {