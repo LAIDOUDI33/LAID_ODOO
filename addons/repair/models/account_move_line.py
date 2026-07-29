24ff1e886a9824a423f1a2d60cadec4818bec6dd
Lucas Gvasalia <lugva@odoo.com>
2026-07-29 15:33:16 +0200

    [IMP] repair: propagate stock move description
    Users have to manually move description to sale order from parts for
    each line. Now description propagates to sale order and invoice
    automatically. It propagates only if it was manually set, and if the
    description does not copy the line name.
    
    task-6374975

--- addons/repair/models/account_move_line.py
+++ addons/repair/models/account_move_line.py
@@ -7,12 +7,14 @@ class AccountMoveLine(models.Model):
     repair_order_id = fields.Many2one(related='move_id.repair_order_id')
     repair_service_line_id = fields.Many2one('repair.service.line', check_company=True, copy=False, index='btree_not_null')
 
-    @api.depends('repair_service_line_id.description')
+    @api.depends('repair_service_line_id.description', 'stock_move_id.description_picking')
     def _compute_name(self):
         super()._compute_name()
         for line in self:
             if line.repair_service_line_id and line.repair_service_line_id.description:
                 line.name = line.translated_product_name + '\n' + line.repair_service_line_id.description
+            elif line.stock_move_id.description_picking_manual and line.translated_product_name != line.stock_move_id.description_picking:
+                line.name = line.translated_product_name + '\n' + line.stock_move_id.description_picking
 
     def _eligible_for_stock_account(self):
         moves = self._get_stock_moves()
