odoo/odoo

[IMP] partner_autocomplete: add autocomplete on multi-ids fields
We want to add the autocompletion feature on the multi-ids fields.

Before, we could search by VAT number or by name.
Now, we can search by name, VAT, or any field in the multi-ids
(including DUNS)

Task-5367705

addons/partner_autocomplete/models/res_partner.py
@@ -89,6 +89,16 @@ class ResPartner(models.Model):
         return iap_data
 
     @api.model
+    def autocomplete_by_field(self, query, query_country_id, field, timeout=15):
+        match field:
+            case "name":
+                return self.autocomplete_by_name(query, query_country_id)
+            case "duns":
+                return self.autocomplete_by_duns(query, query_country_id)
+            case _:
+                return self.autocomplete_by_vat(query, query_country_id)
+
+    @api.model
     def autocomplete_by_name(self, query, query_country_id, timeout=15):
         if query_country_id is False:  # If it's 0, we purposely do not want to filter on the country
             query_country_id = self.env.company.country_id.id
@@ -142,6 +152,19 @@ class ResPartner(models.Model):
                         'country_code': vies_result['countryCode'],
                     })]
             return []
+
+    @api.model
+    def autocomplete_by_duns(self, duns, query_country_id, timeout=15):
+        response, _ = self.env['iap.autocomplete.api']._request_partner_autocomplete('search_by_duns', {
+            'query': duns,
+        }, timeout=timeout)
+        if not response or response.get("error"):
+            return []
+
+        results = []
+        for suggestion in response.get("data"):
+            results.append(self._format_data_company(suggestion))
+        return results
 
     @api.model
     def _process_enriched_response(self, response, error):

addons/partner_autocomplete/static/src/js/partner_autocomplete_additional_identifiers_list.js
@@ -1,0 +1,125 @@
+import { useChildRef, useService } from "@web/core/utils/hooks";
+import { registry } from "@web/core/registry";
+import { _t } from "@web/core/l10n/translation";
+import { AdditionalIdentifiersList, additionalIdentifiersList } from "@web/views/fields/additional_identifiers/additional_identifiers";
+
+import { usePartnerAutocomplete } from "@partner_autocomplete/js/partner_autocomplete_core";
+import { AutoComplete } from "@web/core/autocomplete/autocomplete";
+
+export class PartnerAutoCompleteAdditionalIdentifiersList extends AdditionalIdentifiersList {
+    static template = "partner_autocomplete.PartnerAutoCompleteAdditionalIdentifiersList";
+    static components = {
+        ...AdditionalIdentifiersList.components,
+        AutoComplete,
+    };
+    setup() {
+        super.setup();
+
+        this.orm = useService("orm");
+        this.partnerAutocomplete = usePartnerAutocomplete();
+
+        this.inputRef = useChildRef();
+    }
+
+    async getSearchConfig(request, identifierName, shouldSearchWorldWide) {
+        let defaultCountryId = this.props.record.data?.country_id?.id || false;
+        if (shouldSearchWorldWide) {
+            defaultCountryId = 0;
+        }
+
+        const rules = {
+            'DUNS': {
+                isValid: request && request.length === 9,
+                countryId: 0
+            },
+            'BE_EN': {
+                isValid: request && request.length >= 10,
+                countryId: defaultCountryId
+            },
+            'FR_SIRET': {
+                isValid: request && request.length >= 14,
+                countryId: defaultCountryId
+            },
+            'FR_SIREN': {
+                isValid: request && request.length >= 9,
+                countryId: defaultCountryId
+            },
+        };
+
+        if (rules[identifierName]) {
+            return rules[identifierName];
+        }
+
+        return {
+            isValid: await this.validateSearchTerm(request),
+            countryId: defaultCountryId
+        };
+    }
+
+    async validateSearchTerm(request) {
+        return request && request.length > 2;
+    }
+
+    getSources(identifierName) {
+        return [
+            {
+                options: async (request, shouldSearchWorldWide) => {
+                    const config = await this.getSearchConfig(request, identifierName, shouldSearchWorldWide);
+                    if (!config.isValid) {
+                        return [];
+                    }
+                    const suggestions = await this.partnerAutocomplete.autocomplete(request, config.country_id, identifierName);
+                    return suggestions.map((suggestion) => ({
+                        cssClass: "partner_autocomplete_dropdown_char",
+                        data: suggestion,
+                        label: suggestion.name,
+                        onSelect: () => this.onSelectPartnerAutocompleteOption(suggestion),
+                    }));
+                },
+                optionSlot: "partnerOption",
+                placeholder: _t('Searching Autocomplete...'),
+            },
+        ];
+    }
+
+    async onSelectPartnerAutocompleteOption(option) {
+        let data = await this.partnerAutocomplete.getCreateData(option);
+        if (!data?.company) {
+            return;
+        }
+
+        if (data.logo) {
+            const logoField = this.props.record.resModel === 'res.partner' ? 'image_1920' : 'logo';
+            data.company[logoField] = data.logo;
+        }
+
+        const additionalData = {
+            entity_type: data.company.entity_type,
+            unspsc_codes: data.company.unspsc_codes,
+        };
+        // Delete useless fields before updating record
+        data.company = this.partnerAutocomplete.removeUselessFields(data.company, Object.keys(this.props.record.fields));
+
+        // Update record with retrieved values
+        if (data.company.name) {
+            await this.props.record.update({ name: data.company.name });  // Needed otherwise name it is not saved
+        }
+        await this.props.record.update(data.company);
+
+        // Post message with company info card
+        if (this.props.record.resModel === 'res.partner') {
+            const saved = await this.props.record.save();
+            if (saved && data.isEnrichAccessible) {
+                await this.orm.call("res.partner", "enrich_company_message_post", [this.props.record.resId, additionalData]);
+                this.props.record.load();
+            }
+        }
+    }
+}
+
+export const partnerAutoCompleteAdditionalIdentifiersList = {
+    ...additionalIdentifiersList,
+    component: PartnerAutoCompleteAdditionalIdentifiersList,
+};
+
+registry.category("fields").add("additional_identifiers_list_partner_autocomplete", partnerAutoCompleteAdditionalIdentifiersList);

addons/partner_autocomplete/static/src/js/partner_autocomplete_core.js
@@ -70,14 +70,15 @@ export function usePartnerAutocomplete() {
         return isGST;
     }
 
-    async function autocomplete(value, queryCountryId) {
+    async function autocomplete(value, queryCountryId, field='name') {
         value = value.trim();
-        const isVAT = await isVATNumber(value);
-        if (isVAT){
-        	value = sanitizeVAT(value);
+        if (field !== 'name') {
+            value = sanitizeVAT(value);
+            if (field === 'vat' && !isVATNumber(value)) {
+                return [];
+            }
         }
-        const isGST = isGSTNumber(value);
-        return await getSuggestions(value, isVAT || isGST, queryCountryId);
+        return await getSuggestions(value, field, queryCountryId);
     }
 
     /**
@@ -148,24 +149,22 @@ export function usePartnerAutocomplete() {
      * Use Odoo Autocomplete API to return suggestions
      *
      * @param {string} value
-     * @param {boolean} isVAT
+     * @param {string} field
      * @returns {Promise}
      * @private
      */
-    async function getSuggestions(value, isVAT, queryCountryId) {
-        const method = isVAT ? 'autocomplete_by_vat' : 'autocomplete_by_name';
-
+    async function getSuggestions(value, field, queryCountryId) {
         // Optimization: if the search query starts with the same content as a previous query for
         // which there was no results, there won't be any results for the current query.
         // E.g., if there is no results for query "abc123", there won't be any results for query "abc1234".
-        if (!isVAT && lastNoResultsQuery && value.startsWith(lastNoResultsQuery)) {
+        // if (!field && lastNoResultsQuery && value.startsWith(lastNoResultsQuery)) {
+        if (field === 'name' && lastNoResultsQuery && value.startsWith(lastNoResultsQuery)) {
             return [];
         }
-
         const prom = orm.silent.call(
             'res.partner',
-            method,
-            [value, queryCountryId],
+            'autocomplete_by_field',
+            [value, queryCountryId, field.toLowerCase()],
         );
 
         let suggestions = [];
@@ -176,9 +175,11 @@ export function usePartnerAutocomplete() {
             throw e;
           }
         }
-
-        if (!isVAT && suggestions.length === 0) {
-            lastNoResultsQuery = value;
+        if (suggestions.length === 0) {
+            if (field === 'name') {
+                lastNoResultsQuery = value;
+            }
+            return [];
         }
 
         await Promise.all(suggestions.map(async (suggestion) => {

addons/partner_autocomplete/static/src/js/partner_autocomplete_fieldchar.js
@@ -27,7 +27,7 @@ export class PartnerAutoCompleteCharField extends CharField {
         return request && request.length > 2;
     }
 
-    get sources() {
+    getSources(identifierName) {
         return [
             {
                 options: async (request, shouldSearchWorldWide) => {
@@ -36,7 +36,7 @@ export class PartnerAutoCompleteCharField extends CharField {
                         if (shouldSearchWorldWide){
                         	queryCountryId = 0;
                         }
-                        const suggestions = await this.partnerAutocomplete.autocomplete(request, queryCountryId);
+                        const suggestions = await this.partnerAutocomplete.autocomplete(request, queryCountryId, identifierName);
                         return suggestions.map((suggestion) => ({
                             cssClass: "partner_autocomplete_dropdown_char",
                             data: suggestion,

LAIDOUDI33/LAID_ODOO

addons/partner_autocomplete/models/res_partner.py
@@ -89,6 +89,16 @@ class ResPartner(models.Model):
         return iap_data
 
     @api.model
+    def autocomplete_by_field(self, fieldName, query, query_country_id, timeout=15):
+        match fieldName:
+            case "name":
+                return self.autocomplete_by_name(query, query_country_id)
+            case "duns":
+                return self.autocomplete_by_duns(query, query_country_id)
+            case _:
+                return self.autocomplete_by_vat(query, query_country_id)
+
+    @api.model
     def autocomplete_by_name(self, query, query_country_id, timeout=15):
         if query_country_id is False:  # If it's 0, we purposely do not want to filter on the country
             query_country_id = self.env.company.country_id.id
@@ -142,6 +152,22 @@ class ResPartner(models.Model):
                         'country_code': vies_result['countryCode'],
                     })]
             return []
+
+    @api.model
+    def autocomplete_by_duns(self, duns, query_country_id, timeout=15):
+        query_country_id = query_country_id or self.env.company.country_id.id
+        query_country_code = self.env['res.country'].browse(query_country_id).code
+        response, _ = self.env['iap.autocomplete.api']._request_partner_autocomplete('search_by_duns', {
+            'query': duns,
+            'query_country_code': query_country_code,
+        }, timeout=timeout)
+        if not response or response.get("error"):
+            return []
+
+        results = []
+        for suggestion in response.get("data"):
+            results.append(self._format_data_company(suggestion))
+        return results
 
     @api.model
     def _process_enriched_response(self, response, error):

addons/partner_autocomplete/static/src/js/partner_autocomplete_additional_identifiers_list.js
@@ -1,0 +1,125 @@
+import { useChildRef, useService } from "@web/core/utils/hooks";
+import { registry } from "@web/core/registry";
+import { _t } from "@web/core/l10n/translation";
+import { AdditionalIdentifiersList, additionalIdentifiersList } from "@web/views/fields/additional_identifiers/additional_identifiers";
+
+import { usePartnerAutocomplete } from "@partner_autocomplete/js/partner_autocomplete_core";
+import { AutoComplete } from "@web/core/autocomplete/autocomplete";
+
+export class PartnerAutoCompleteAdditionalIdentifiersList extends AdditionalIdentifiersList {
+    static template = "partner_autocomplete.PartnerAutoCompleteAdditionalIdentifiersList";
+    static components = {
+        ...AdditionalIdentifiersList.components,
+        AutoComplete,
+    };
+    setup() {
+        super.setup();
+
+        this.orm = useService("orm");
+        this.partnerAutocomplete = usePartnerAutocomplete();
+
+        this.inputRef = useChildRef();
+    }
+
+    async getSearchConfig(fieldName, request, shouldSearchWorldWide) {
+        let defaultCountryId = this.props.record.data?.country_id?.id || false;
+        if (shouldSearchWorldWide) {
+            defaultCountryId = 0;
+        }
+
+        const rules = {
+            'DUNS': {
+                isValid: request && request.length === 9,
+                countryId: 0
+            },
+            'BE_EN': {
+                isValid: request && request.length >= 10,
+                countryId: defaultCountryId
+            },
+            'FR_SIRET': {
+                isValid: request && request.length >= 14,
+                countryId: defaultCountryId
+            },
+            'FR_SIREN': {
+                isValid: request && request.length >= 9,
+                countryId: defaultCountryId
+            },
+        };
+
+        if (rules[fieldName]) {
+            return rules[fieldName];
+        }
+
+        return {
+            isValid: await this.validateSearchTerm(request),
+            countryId: defaultCountryId
+        };
+    }
+
+    async validateSearchTerm(request) {
+        return request && request.length > 2;
+    }
+
+    getSources(fieldName) {
+        return [
+            {
+                options: async (request, shouldSearchWorldWide) => {
+                    const config = await this.getSearchConfig(fieldName, request, shouldSearchWorldWide);
+                    if (!config.isValid) {
+                        return [];
+                    }
+                    const suggestions = await this.partnerAutocomplete.autocomplete(fieldName, request, config.country_id);
+                    return suggestions.map((suggestion) => ({
+                        cssClass: "partner_autocomplete_dropdown_char",
+                        data: suggestion,
+                        label: suggestion.name,
+                        onSelect: () => this.onSelectPartnerAutocompleteOption(suggestion),
+                    }));
+                },
+                optionSlot: "partnerOption",
+                placeholder: _t('Searching Autocomplete...'),
+            },
+        ];
+    }
+
+    async onSelectPartnerAutocompleteOption(option) {
+        let data = await this.partnerAutocomplete.getCreateData(option);
+        if (!data?.company) {
+            return;
+        }
+
+        if (data.logo) {
+            const logoField = this.props.record.resModel === 'res.partner' ? 'image_1920' : 'logo';
+            data.company[logoField] = data.logo;
+        }
+
+        const additionalData = {
+            entity_type: data.company.entity_type,
+            unspsc_codes: data.company.unspsc_codes,
+        };
+        // Delete useless fields before updating record
+        data.company = this.partnerAutocomplete.removeUselessFields(data.company, Object.keys(this.props.record.fields));
+
+        // Update record with retrieved values
+        if (data.company.name) {
+            await this.props.record.update({ name: data.company.name });  // Needed otherwise name it is not saved
+        }
+        await this.props.record.update(data.company);
+
+        // Post message with company info card
+        if (this.props.record.resModel === 'res.partner') {
+            const saved = await this.props.record.save();
+            if (saved && data.isEnrichAccessible) {
+                await this.orm.call("res.partner", "enrich_company_message_post", [this.props.record.resId, additionalData]);
+                this.props.record.load();
+            }
+        }
+    }
+}
+
+export const partnerAutoCompleteAdditionalIdentifiersList = {
+    ...additionalIdentifiersList,
+    component: PartnerAutoCompleteAdditionalIdentifiersList,
+};
+
+registry.category("fields").add("additional_identifiers_list_partner_autocomplete", partnerAutoCompleteAdditionalIdentifiersList);

addons/partner_autocomplete/static/src/js/partner_autocomplete_core.js
@@ -63,14 +63,14 @@ export function usePartnerAutocomplete() {
         return isGST;
     }
 
-    async function autocomplete(value, queryCountryId) {
+    async function autocomplete(fieldName, value, queryCountryId) {
         value = value.trim();
-        const isVAT = await scope.run(isVATNumber, value);
-        if (isVAT) {
-            value = sanitizeVAT(value);
+        if (fieldName === 'vat') {
+            if (!isGSTNumber(value) && !isVATNumber(value)) {
+                return [];
+            }
         }
-        const isGST = isGSTNumber(value);
-        return await scope.run(getSuggestions, value, isVAT || isGST, queryCountryId);
+        return await scope.run(getSuggestions, fieldName, value, queryCountryId);
     }
 
     /**
@@ -137,22 +137,21 @@ export function usePartnerAutocomplete() {
     /**
      * Use Odoo Autocomplete API to return suggestions
      *
+     * @param {string} fieldName
      * @param {string} value
-     * @param {boolean} isVAT
+     * @param {int} queryCountryId
      * @returns {Promise}
      * @private
      */
-    async function getSuggestions(value, isVAT, queryCountryId) {
-        const method = isVAT ? "autocomplete_by_vat" : "autocomplete_by_name";
-
+    async function getSuggestions(fieldName, value, queryCountryId) {
         // Optimization: if the search query starts with the same content as a previous query for
         // which there was no results, there won't be any results for the current query.
         // E.g., if there is no results for query "abc123", there won't be any results for query "abc1234".
-        if (!isVAT && lastNoResultsQuery && value.startsWith(lastNoResultsQuery)) {
+        if (fieldName === 'name' && lastNoResultsQuery && value.startsWith(lastNoResultsQuery)) {
             return [];
         }
 
-        const prom = orm.silent.call("res.partner", method, [value, queryCountryId]);
+        const prom = orm.silent.call('res.partner', 'autocomplete_by_field', [fieldName.toLowerCase(), value, queryCountryId]);
 
         let suggestions = [];
         try {
@@ -162,9 +161,11 @@ export function usePartnerAutocomplete() {
                 throw e;
             }
         }
-
-        if (!isVAT && suggestions.length === 0) {
-            lastNoResultsQuery = value;
+        if (suggestions.length === 0) {
+            if (fieldName === 'name') {
+                lastNoResultsQuery = value;
+            }
+            return [];
         }
 
         await Promise.all(

addons/partner_autocomplete/static/src/js/partner_autocomplete_fieldchar.js
@@ -27,7 +27,7 @@ export class PartnerAutoCompleteCharField extends CharField {
         return request && request.length > 2;
     }
 
-    get sources() {
+    getSources(fieldName) {
         return [
             {
                 options: async (request, shouldSearchWorldWide) => {
@@ -36,7 +36,7 @@ export class PartnerAutoCompleteCharField extends CharField {
                         if (shouldSearchWorldWide){
                         	queryCountryId = 0;
                         }
-                        const suggestions = await this.partnerAutocomplete.autocomplete(request, queryCountryId);
+                        const suggestions = await this.partnerAutocomplete.autocomplete(fieldName, request, queryCountryId);
                         return suggestions.map((suggestion) => ({
                             cssClass: "partner_autocomplete_dropdown_char",
                             data: suggestion,