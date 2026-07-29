57d0c9baaae6da91ac045357f36043542c95503c
sawer <sawer@odoo.com>
2026-07-29 16:48:13 +0200

    [FIX] point_of_sale: don't exclude archived sub-combinations
    Steps to reproduce
    --------------------
    
    - Install point_of_sale module;
    - Create a new product available in PoS;
    - Add a variant attribute with three values;
    - Modify one of the variant to use a second value (add one of the
    two other values to product_template_variant_value_ids);
    - Archive this variant;
    - Open a PoS session where the product is available;
    - Select the new product;
    - You cannot select the two values added on the archived variant
    (error message: This option or combination of options is not
    available).
    
    Why is it happening
    --------------------
    
    In the _isArchivedCombination function, we iterate over all
    the archived combinations for the product and create a
    sub-combination containing the values shared with the currently
    selected combination.
    
    If the sub-combination has the same lenght as the currently
    selected combination, it is considered archived.
    
    As a result, if the selected combination is a sub-combination
    of an archived one, it will be considered archived.
    
    opw-6413713

--- addons/point_of_sale/static/src/app/models/product_template.js
+++ addons/point_of_sale/static/src/app/models/product_template.js
@@ -289,7 +289,10 @@ export class ProductTemplate extends Base {
                 );
                 excludedPTAV.add(disablePTAV);
             }
-            if (ptavCommon.length === attributeValueIds.length) {
+            if (
+                ptavCommon.length === attributeValueIds.length &&
+                attributeValueIds.length === archivedCombination.length
+            ) {
                 isCombinationArchived = true;
             }
         }
