6e2630f6460fb0804fd3c1029d6cc151b324d6f9
Youssef Bashandy <yoba@odoo.com>
2026-07-29 17:51:32 +0200

    [FIX] resource : Create a WS when company's WS is two weeks calendar
    Steps to reproduce:
    	- Install Employee
    	- Create a 2-week working schedule and set it as the company default
    	- Try to create a new working schedule
    	- Notice when you click save a ValidationErroe arise
    
    Cause:
    Two parts where causing this. First when creating a new calendar and
    we try to fetch default attendances we don't set the sequence in the
    newly created attendances
    
    https://github.com/odoo/odoo/blob/b51dc298fd63d9586d0b8d7cff59764b0dee5cae/addons/resource/models/resource_calendar.py#L735-L749
    
    so it will get the default value which is 10 so when calling onchange for
    the attendance_ids_1st_week and attendance_ids_2nd_week each attendance
    will be set to the odd_week_seq
    
    https://github.com/odoo/odoo/blob/b51dc298fd63d9586d0b8d7cff59764b0dee5cae/addons/resource/models/resource_calendar.py#L184-L200
    
    which will then make this condition fail
    
    https://github.com/odoo/odoo/blob/b51dc298fd63d9586d0b8d7cff59764b0dee5cae/addons/resource/models/resource_calendar.py#L615-L616
    
    Second part was related to the  when saving,
    its value won't be passed to the vals_list in  as when
    we read the values to be changed we ignore readonly fields and since
    two_weeks_calendar was used in invisible condition but isn't defined
    in a separate  the view create a tag for it
    
    `
    <field name='two_weeks_calendar' invisible='True' readonly='True' data-used-by='invisible='flexible_hours or not two_weeks_calendar' (page,working_hours)' on_change='1'/>
    `
    
    This tag would be readonly by default so when the ArchParser
    gets each field's info it puts  as a readonly
    field and ignore it in the creation values
    
    https://github.com/odoo/odoo/blob/b51dc298fd63d9586d0b8d7cff59764b0dee5cae/addons/web/static/src/views/fields/field.js#L276-L279
    
    which then fails this condition and pass all the 2 weeks attendances in the else condition
    
    https://github.com/odoo/odoo/blob/b51dc298fd63d9586d0b8d7cff59764b0dee5cae/addons/resource/models/resource_calendar.py#L134-L138
    
    After fixing this another bug was found where if you saved the calendar
    the attendances will disappear. This was happening when we create the
    resource.calendar.attendance records it will call the inverse method
    of the attendance_ids_1st_week and attendance_ids_2nd_week where they are
    still not computed so it will set attendance_ids to empty
    
    https://github.com/odoo/odoo/blob/b51dc298fd63d9586d0b8d7cff59764b0dee5cae/addons/resource/models/resource_calendar.py#L152-L156
    
    so after when computing the two weeks attendance it will be empty
    as well and it will disappear.
    
    Last bug was if after creating this you tried to switch the calendar
    type it will call the same validation error mentioned earlier. As when
    calling _get_default_attendance_ids it will try to create attendances
    from the company's default working schedule which will have a conflict
    since the company's schedule is 2-weeks schedule and we are switching
    our schedule to 1-week schedule so we are gonna have attendances for 2-weeks
    in 1-week schedule so it will fail the same condition
    
    https://github.com/odoo/odoo/blob/b51dc298fd63d9586d0b8d7cff59764b0dee5cae/addons/resource/models/resource_calendar.py#L615-L616
    
    Fix:
    To fix those issues we needed to set the sequence values when copying
    the data of the company's schedule when computing the default values.
    
    Also we need to skip the inverse method when we are still upon creating the records
    and to do so we are passing a context in the create method to skip
    the inverse.
    
    Last we need to check for the difference between the schedule type and the
    company's schedule when fetching the default attendances.
    
    opw-6374237

--- addons/resource/models/resource_calendar.py
+++ addons/resource/models/resource_calendar.py
@@ -151,7 +151,7 @@ class ResourceCalendar(models.Model):
 
     def _inverse_two_weeks_calendar(self):
         for calendar in self:
-            if not calendar.two_weeks_calendar:
+            if not calendar.two_weeks_calendar or self.env.context.get('skip_inverse'):
                 continue
             calendar.attendance_ids = calendar.attendance_ids_1st_week + calendar.attendance_ids_2nd_week
 
@@ -282,6 +282,11 @@ class ResourceCalendar(models.Model):
     # --------------------------------------------------
     # Overrides
     # --------------------------------------------------
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super(ResourceCalendar, self.with_context(skip_inverse=True)).create(vals_list)
+        return res
 
     def copy_data(self, default=None):
         vals_list = super().copy_data(default=default)
@@ -734,7 +739,10 @@ class ResourceCalendar(models.Model):
 
     def _get_default_attendance_ids(self, company_id=None):
         """ return a copy of the company's calendar attendance or default 40 hours/week """
-        if company_id and (attendances := company_id.resource_calendar_id.attendance_ids):
+        if (
+            company_id and (attendances := company_id.resource_calendar_id.attendance_ids)
+            and (not self or company_id.resource_calendar_id.two_weeks_calendar == self.two_weeks_calendar)
+        ):
             return [
                 Command.create({
                     'name': attendance.name,
@@ -744,6 +752,7 @@ class ResourceCalendar(models.Model):
                     'hour_to': attendance.hour_to,
                     'day_period': attendance.day_period,
                     'display_type': attendance.display_type,
+                    'sequence': attendance.sequence,
                 })
                 for attendance in attendances
             ]
