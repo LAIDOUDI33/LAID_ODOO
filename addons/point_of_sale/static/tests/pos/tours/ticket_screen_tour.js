odoo/odoo

[FIX] point_of_sale: don't crash on backend-only widgets in PoS UI
Steps to reproduce:
- Open any PoS session
- Click on Orders and filter by "Paid" orders
- Select an order and click on "Details"
- Open the "Payments" tab and click on the "View" button of a payment
- Click on the payment method link

Traceback:
OwlError: The following error occurred in onWillStart: "Cannot find
key "pos_payment_provider_cards" in the "view_widgets" registry"

Backend views opened from within the PoS UI (order details dialog,
drill-down on many2one links, ...) share their arch with the backend,
but `point_of_sale._assets_pos` excludes everything under
`static/src/backend/`, so widgets defined there (e.g.
`pos_payment_provider_cards`, `lna_checklist`,
`point_of_sale_test_epos`) are never registered in the PoS UI. Unlike
missing field widgets, which fall back to the default widget with a
warning, an unknown `<widget>` node makes the whole view crash since
`Widget.parseWidgetNode` reads the registry without a fallback.

Those widgets are backend configuration helpers that are irrelevant in
a PoS session, so instead of bundling each of them (and any future one)
in the PoS assets, patch `Widget.parseWidgetNode` in the PoS bundle to
skip unknown widgets with a warning, like missing field widgets do.
This covers the form, list and kanban arch parsers at once.

opw-6382156

addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
@@ -7,7 +7,8 @@ import * as TicketScreen from "@point_of_sale/../tests/pos/tours/utils/ticket_sc
 import * as Order from "@point_of_sale/../tests/generic_helpers/order_widget_util";
 import * as Chrome from "@point_of_sale/../tests/pos/tours/utils/chrome_util";
 import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
-import { inLeftSide } from "@point_of_sale/../tests/pos/tours/utils/common";
+import { inLeftSide, selectButton } from "@point_of_sale/../tests/pos/tours/utils/common";
+import * as BackendUtils from "@point_of_sale/../tests/pos/tours/utils/backend_utils";
 import { registry } from "@web/core/registry";
 import * as OfflineUtil from "@point_of_sale/../tests/generic_helpers/offline_util";
 import * as ProductConfiguratorPopup from "@point_of_sale/../tests/pos/tours/utils/product_configurator_util";
@@ -660,5 +661,30 @@ registry.category("web_tour.tours").add("test_not_available_pricelist_not_set_on
             PaymentScreen.clickPaymentMethod("Bank"),
             PaymentScreen.clickValidate(),
             ReceiptScreen.isShown(),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("test_paid_order_payment_method_drilldown", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            ProductScreen.clickDisplayedProduct("Desk Pad"),
+            ProductScreen.clickPayButton(),
+            PaymentScreen.clickPaymentMethod("Cash"),
+            PaymentScreen.clickValidate(),
+            ReceiptScreen.clickNextOrder(),
+            Chrome.clickOrders(),
+            TicketScreen.selectFilter("Paid"),
+            TicketScreen.selectOrder("Paid"),
+            ProductScreen.clickReview(),
+            selectButton("Details"),
+            BackendUtils.clickNotebookTab("Payments"),
+            BackendUtils.openListRowFormView("payment_ids"),
+            BackendUtils.followMany2oneLink("payment_method_id"),
+            {
+                content: "The payment method form is displayed without crashing",
+                trigger: ".o_form_view .o_field_widget[name=payment_method_type]",
+            },
         ].flat(),
 });

LAIDOUDI33/LAID_ODOO

X-original-commit: 9b7d445ec67c5a750aa3a7e0acae4c4eafa9ee65

addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
@@ -7,7 +7,8 @@ import * as TicketScreen from "@point_of_sale/../tests/pos/tours/utils/ticket_sc
 import * as Order from "@point_of_sale/../tests/generic_helpers/order_widget_util";
 import * as Chrome from "@point_of_sale/../tests/pos/tours/utils/chrome_util";
 import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
-import { inLeftSide } from "@point_of_sale/../tests/pos/tours/utils/common";
+import { inLeftSide, selectButton } from "@point_of_sale/../tests/pos/tours/utils/common";
+import * as BackendUtils from "@point_of_sale/../tests/pos/tours/utils/backend_utils";
 import { registry } from "@web/core/registry";
 import * as OfflineUtil from "@point_of_sale/../tests/generic_helpers/offline_util";
 import * as ProductConfiguratorPopup from "@point_of_sale/../tests/pos/tours/utils/product_configurator_util";
@@ -673,5 +674,30 @@ registry.category("web_tour.tours").add("test_not_available_pricelist_not_set_on
             PaymentScreen.clickPaymentMethod("Bank"),
             PaymentScreen.clickValidate(),
             FeedbackScreen.isShown(),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("test_paid_order_payment_method_drilldown", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            ProductScreen.clickDisplayedProduct("Desk Pad"),
+            ProductScreen.clickPayButton(),
+            PaymentScreen.clickPaymentMethod("Cash"),
+            PaymentScreen.clickValidate(),
+            FeedbackScreen.clickNextOrder(),
+            Chrome.clickOrders(),
+            TicketScreen.selectFilter("Paid"),
+            TicketScreen.selectOrder("Paid"),
+            ProductScreen.clickReview(),
+            selectButton("Details"),
+            BackendUtils.clickNotebookTab("Payments"),
+            BackendUtils.openListRowFormView("payment_ids"),
+            BackendUtils.followMany2oneLink("payment_method_id"),
+            {
+                content: "The payment method form is displayed without crashing",
+                trigger: ".o_form_view .o_field_widget[name=payment_method_type]",
+            },
         ].flat(),
 });