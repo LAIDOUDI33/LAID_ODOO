odoo/odoo

[IMP] google_calendar: multi calendar sync
The calendar.calendar model is extended with sync logic, sharing the base sync
class with events. Each calendar also holds its individual sync token so that we
only update when there is new data from Google.

## Sync Flow

1. Synchronize all calendars

Before starting the usual sync flow in 'res.users', we first make a call to
`/calendarList` to fetch all the user's calendars. A sync token for this call is
stored in the user settings, similar to the 'global' sync token before. All
calendars that do not have a google_id set are then posted to Google.

Unlike for events, the calendar endpoints do not return an 'updated' timestamp,
meaning we can't rely on last write date to determine which update wins in case
of a conflict - we have to decide which side is authoritative.
In this case -> Odoo

After this part of the request is done, we manually commit the changes. This is
because the google api methods are decorated with @after_commit so that we
don't send data if the sync process fails for some reason. We need all the
calendars to be synchronized when we sync events. That's why we call
`env.cr.commit()`

Small technical limitation:
Besides the events themselves, we currently only sync the name (summary) field
with google. When this is changed on google however, it does not invalidate the
sync token, meaning we are not notified of this change (changing some other
field on google, such as calendar color, correctly invalidates the sync token)

2. Synchronize the events

Google handles moving events between two calendars -> It returns a canceled
record for the original calendar, and a new one for the moved one. This means we
have to split the process into several parts instead of running the current
flow for each calendar:

2.1: Get events for all calendars from Google

Paths always used to be `.../primary/...` -> now `.../{calendar_id}/...`.
Otherwise the same as before.

2.2 Google -> Odoo

First, determine which should be acti

addons/google_calendar/static/src/views/google_calendar/common/google_calendar_common_popover.js
@@ -3,6 +3,8 @@ import { patch } from "@web/core/utils/patch";
 
 patch(AttendeeCalendarCommonPopover.prototype, {
     get isEventArchivable() {
-        return super.isEventArchivable || (this.isCurrentUserOrganizer && this.props.record.rawRecord.google_id);
+        return super.isEventArchivable || (
+            (this.isCurrentUserOrganizer || this.props.record.isInUserCalendar) && this.props.record.rawRecord.google_id
+        );
     },
 });

addons/google_calendar/tests/test_sync_calendars_google2odoo.py
@@ -1,0 +1,134 @@
+from datetime import date, datetime, UTC
+from unittest.mock import patch
+
+from odoo.addons.google_calendar.models.res_users import ResUsers
+from odoo.addons.google_calendar.tests.test_sync_common import TestSyncGoogle, patch_api
+from odoo.addons.google_calendar.utils.google_calendar import GoogleCalendar
+from odoo.addons.google_calendar.utils.google_event import GoogleEvent
+from odoo.tests.common import tagged
+
+
+@patch.object(ResUsers, '_get_google_calendar_token', lambda user: 'dummy-token')
+@tagged('at_install', '-post_install')
+class TestSyncCalendarsGoogle2Odoo(TestSyncGoogle):
+
+    @patch_api
+    def test_g2o_new_calendar_is_created_and_does_not_create_duplicate_on_google(self):
+        google_calendars = GoogleCalendar([{'id': 'google-new-1', 'summary': 'My New Calendar', 'accessRole': 'owner'}])
+
+        self.organizer_user.calendar_ids.with_user(self.organizer_user)._sync_calendars_google2odoo(google_calendars)
+
+        calendar = self.env['calendar.calendar'].search([('google_id', '=', 'google-new-1')], limit=1)
+        self.assertTrue(calendar)
+        self.assertEqual(calendar.name, 'My New Calendar')
+        self.assertGoogleEventNotInserted()
+
+    def test_g2o_deleted_calendar_is_unlinked(self):
+        """
+        A GoogleCalendar whose 'deleted' flag is truthy must cause the matching non-primary Odoo record to be removed.
+        """
+        odoo_cal = self.create_calendar(name='To Delete', google_id='google-del-1')
+        google_calendars = GoogleCalendar([{
+            'id': 'google-del-1',
+            'summary': 'To Delete',
+            'deleted': True,
+        }])
+
+        self.organizer_user.calendar_ids.with_user(self.organizer_user)._sync_calendars_google2odoo(google_calendars)
+
+        self.assertFalse(odoo_cal.exists(), "Calendar flagged deleted on Google should be removed from Odoo")
+
+    def test_g2o_primary_calendar_is_not_deleted(self):
+        """
+        The primary Odoo calendar must never be deleted, even if the Google payload somehow marks the calendar as deleted.
+        """
+        google_calendars = GoogleCalendar([{
+            'id': 'primary-gid',
+            'summary': 'Primary',
+            'deleted': True,
+            'primary': True,
+        }])
+
+        self.organizer_user.calendar_ids.with_user(self.organizer_user)._sync_calendars_google2odoo(google_calendars)
+
+        self.assertTrue(
+            self.organizer_user.primary_calendar.exists(),
+            "Primary calendar must never be deleted even when flagged deleted on Google"
+        )
+
+    def test_g2o_updated_calendar_writes_changed_fields(self):
+        odoo_cal = self.create_calendar(name='Old Name', google_id='google-upd-1', need_sync=False)
+        google_calendars = GoogleCalendar([{'id': 'google-upd-1', 'summary': 'New Name From Google'}])
+
+        self.env['calendar.calendar']._sync_calendars_google2odoo(google_calendars)
+
+        self.assertEqual(
+            odoo_cal.name, 'New Name From Google',
+            "Calendar name should be updated from Google when Odoo has no pending changes",
+        )
+
+    def test_g2o_updated_calendar_skips_when_odoo_has_pending_changes(self):
+        odoo_cal = self.create_calendar(name='Local Name', google_id='gid', need_sync=True)
+        google_calendars = GoogleCalendar([{'id': 'gid', 'summary': 'Google Name'}])
+
+        self.env['calendar.calendar']._sync_calendars_google2odoo(google_calendars)
+
+        self.assertEqual(
+            odoo_cal.name, 'Local Name',
+            "Calendar with pending local changes should NOT be overwritten by Google",
+        )
+
+    @patch_api
+    @patch.object(ResUsers, '_sync_request')
+    def test_g2o_event_moved_to_a_different_calendar_in_google(self, mock_sync_request):
+        # Create an event in the primary calendar
+        google_id = '1'
+        event = self.env['calendar.event'].with_user(self.organizer_user).create({
+            'name': 'Hello world',
+            'start': date(2020, 1, 6),
+            'stop': date(2020, 1, 6),
+            'google_id': google_id,
+            'user_id': self.organizer_user.id,
+            'need_sync': False,
+            'partner_ids': [(6, 0, self.organizer_user.partner_id.ids)]
+        })
+
+        # Define mock return values for the '_sync_request' method.
+        def mock_sync(google_service, event_id=None, calendar=None):
+            if calendar.is_primary:
+                return {
+                    # Event was removed from the primary calendar
+                    'events': GoogleEvent([{'id': google_id, 'status': 'cancelled'}]),
+                    'default_reminders': (),
+                    'full_sync': False,
+                }
+            # Event respawned in a secondary calendar
+            elif calendar.google_id == self.secondary_calendar.google_id:
+                return {
+                    'events': GoogleEvent([{
+                        'id': google_id,
+                        'description': 'desc',
+                        "updated": datetime.now().replace(tzinfo=UTC).isoformat(),
+                        'organizer': {'email': 'odoocalendarref@gmail.com', 'self': True},
+                        'summary': 'Pricing new update',
+                        'visibility': 'public',
+                        'attendees': [],
+                        'reminders': {'useDefault': True},
+                        'start': {
+                            'dateTime': '2020-01-13T16:55:00+01:00',
+                            'timeZone': 'Europe/Brussels'
+                        },
+                        'end': {
+                            'dateTime': '2020-01-13T19:55:00+01:00',
+                            'timeZone': 'Europe/Brussels'
+                        },
+                    }]),
+                    'default_reminders': (),
+                    'full_sync': False,
+                }
+            raise Exception("Unexpected calendar")
+
+        mock_sync_request.side_effect = mock_sync
+        self.organizer_user.with_user(self.organizer_user)._sync_google_calendar(self.google_service)
+        self.assertTrue(event.exists(), "Event should not be deleted because of the move")
+        self.assertEqual(event.calendar_id, self.secondary_calendar)

addons/google_calendar/tests/test_sync_calendars_odoo2google.py
@@ -1,0 +1,126 @@
+from datetime import datetime
+from unittest.mock import patch
+
+from odoo.addons.google_calendar.models.res_users import ResUsers
+from odoo.addons.google_calendar.tests.test_sync_common import TestSyncGoogle, patch_api
+from odoo.addons.google_calendar.utils.google_event import GoogleEvent
+from odoo.tests.common import tagged
+
+
+@patch.object(ResUsers, '_get_google_calendar_token', lambda user: 'dummy-token')
+@tagged('at_install', '-post_install')
+class TestSyncCalendarsGoogle2Odoo(TestSyncGoogle):
+
+    @patch_api
+    def test_o2g_skip_when_sync_not_active(self):
+        """Nothing must be sent to Google when the user's sync status is not sync_active."""
+        self.organizer_user.pause_google_synchronization()
+        cal = self.create_calendar(name='My Calendar', need_sync=True)
+        cal._sync_calendars_odoo2google(self.google_service)
+        self.assertGoogleAPINotCalled()
+
+    @patch_api
+    def test_o2g_new_non_primary_calendar_calls_insert(self):
+        """Creating a new calendar must call _google_calendar_insert."""
+        cal = self.create_calendar(name='New Calendar')
+        self.assertGoogleCalendarInserted(cal._google_values())
+
+    @patch_api
+    def test_o2g_existing_synced_calendar_calls_patch(self):
+        """Editing synchronized calendar must call _google_calendar_patch."""
+        self.organizer_user.primary_calendar._sync_calendars_odoo2google(self.google_service)
+        self.organizer_user.primary_calendar.name = 'New Name'
+        self.assertGoogleCalendarPatched(
+            self.organizer_user.primary_calendar.id,
+            self.organizer_user.primary_calendar._google_values()
+        )
+
+    @patch_api
+    def test_o2g_moving_calendar_in_odoo_moves_event_in_google(self):
+        # Create an event in the primary calendar
+        google_id = '1'
+        event = self.env['calendar.event'].with_user(self.organizer_user).create({
+            'name': 'Hello world',
+            'google_id': google_id,
+            'user_id': self.organizer_user.id,
+            'need_sync': False,
+            'partner_ids': [(6, 0, self.organizer_user.partner_id.ids)]
+        })
+        # Move the event and assert that the move was called
+        event.calendar_id = self.secondary_calendar
+        self.assertGoogleEventMoved(google_id, "primary", self.secondary_calendar.google_id)
+        event.calendar_id = self.organizer_user.primary_calendar
+        self.assertGoogleEventMoved(google_id, self.secondary_calendar.google_id, "primary")
+
+    @patch_api
+    @patch.object(ResUsers, '_sync_request')
+    def test_o2g_moving_calendar_in_odoo_when_paused_moves_event_in_google_after_unpause(self, mock_sync_request):
+        google_id = '2'
+        event = self.env['calendar.event'].with_user(self.organizer_user).create({
+            'name': 'Hello world',
+            'google_id': google_id,
+            'user_id': self.organizer_user.id,
+            'need_sync': False,
+            'partner_ids': [(6, 0, self.organizer_user.partner_id.ids)]
+        })
+
+        # Define mock return values for the '_sync_request' method.
+        mock_sync_request.return_value = {
+            'events': GoogleEvent([]),
+            'default_reminders': (),
+            'full_sync': False,
+        }
+
+        # Pause the synchronization and move the event
+        self.organizer_user.sudo().pause_google_synchronization()
+        event.calendar_id = self.secondary_calendar
+
+        # With the synchronization paused, manually call the synchronization to simulate the page refresh.
+        self.organizer_user.sudo()._sync_google_calendar(self.google_service)
+        self.assertGoogleEventNotMoved()
+
+        # Unpause and call the calendar synchronization. Ensure the event was moved on Google side.
+        self.organizer_user.sudo().unpause_google_synchronization()
+        self.organizer_user.with_user(self.organizer_user).sudo()._sync_google_calendar(self.google_service)
+        self.assertGoogleEventMoved(google_id, "primary", self.secondary_calendar.google_id)
+
+    @patch_api
+    def test_o2g_changing_calendar_organizer_removes_and_recreates_calendar_in_google(self):
+        google_id = '3'
+        event = self.env['calendar.event'].with_user(self.organizer_user).create({
+            'name': 'Event',
+            'start': datetime(2020, 1, 15, 8, 0),
+            'stop': datetime(2020, 1, 15, 18, 0),
+            'google_id': google_id,
+            'user_id': self.organizer_user.id,
+            'need_sync': True,
+            'partner_ids': [(6, 0, self.organizer_user.partner_id.ids)]
+        })
+        insert_common_data = {
+            'start': {'dateTime': '2020-01-15T08:00:00+00:00', 'date': None},
+            'end': {'dateTime': '2020-01-15T18:00:00+00:00', 'date': None},
+            'summary': 'Event',
+            'description': event.description,
+            'location': '',
+            'guestsCanModify': True,
+            'extendedProperties': {'shared': {'%s_odoo_id' % self.env.cr.dbname: event.id}},
+            'reminders': {'overrides': [{'method': 'popup', 'minutes': 15}], 'useDefault': False},
+            'attendees': [{'email': 'o.o@example.com', 'responseStatus': 'accepted'}],
+            'conferenceData': None,
+            'transparency': 'opaque',
+        }
+        self.assertGoogleEventInserted({
+            'id': '3',
+            'organizer': {'email': 'o.o@example.com', 'self': True},
+            **insert_common_data,
+        })
+
+        google_id = event.google_id
+        event.user_id = self.attendee_user
+        self.assertGoogleEventDeleted(google_id)
+        self.assertFalse(event.google_id)
+        self.assertEqual(event.calendar_id, self.attendee_user.primary_calendar)
+
+        # Event should be inserted again
+        event._sync_odoo2google(self.google_service)
+        self.assertEqual(len(self._gsync_insert_values), 2)

LAIDOUDI33/LAID_ODOO

addons/google_calendar/static/src/views/google_calendar/common/google_calendar_common_popover.js
@@ -3,6 +3,8 @@ import { patch } from "@web/core/utils/patch";
 
 patch(AttendeeCalendarCommonPopover.prototype, {
     get isEventArchivable() {
-        return super.isEventArchivable || (this.isCurrentUserOrganizer && this.props.record.rawRecord.google_id);
+        return super.isEventArchivable || (
+            (this.isCurrentUserOrganizer || this.props.record.isInUserCalendars) && this.props.record.rawRecord.google_id
+        );
     },
 });

addons/google_calendar/tests/test_sync_calendars_google2odoo.py
@@ -1,0 +1,135 @@
+from datetime import date, datetime, UTC
+from unittest.mock import patch
+
+from odoo.addons.google_calendar.models.res_users import ResUsers
+from odoo.addons.google_calendar.tests.test_sync_common import TestSyncGoogle, patch_api
+from odoo.addons.google_calendar.utils.google_calendar import GoogleCalendar
+from odoo.addons.google_calendar.utils.google_event import GoogleEvent
+from odoo.tests.common import tagged
+
+
+@patch.object(ResUsers, '_get_google_calendar_token', lambda user: 'dummy-token')
+@tagged('at_install', '-post_install')
+class TestSyncCalendarsGoogle2Odoo(TestSyncGoogle):
+
+    @patch_api
+    def test_g2o_new_calendar_is_created_and_does_not_create_duplicate_on_google(self):
+        google_calendars = GoogleCalendar([{'id': 'google-new-1', 'summary': 'My New Calendar', 'accessRole': 'owner'}])
+
+        self.organizer_user.calendar_ids.with_user(self.organizer_user)._sync_calendars_google2odoo(google_calendars)
+
+        calendar = self.env['calendar.calendar'].search([('google_id', '=', 'google-new-1')], limit=1)
+        self.assertTrue(calendar)
+        self.assertEqual(calendar.calendar_user_ids[0].label, 'My New Calendar')
+        self.assertGoogleEventNotInserted()
+
+    def test_g2o_deleted_calendar_is_unlinked(self):
+        """
+        A GoogleCalendar whose 'deleted' flag is truthy must cause the matching
+        non-primary Odoo record to be removed if it has no users.
+        """
+        odoo_cal = self.create_calendar(name='To Delete', google_id='google-del-1')
+        google_calendars = GoogleCalendar([{
+            'id': 'google-del-1',
+            'summary': 'To Delete',
+            'deleted': True,
+        }])
+
+        self.organizer_user.calendar_ids.with_user(self.organizer_user)._sync_calendars_google2odoo(google_calendars)
+
+        self.assertFalse(odoo_cal.active, "Calendar flagged deleted on Google should be archived in Odoo")
+
+    def test_g2o_primary_calendar_is_not_deleted(self):
+        """
+        The primary Odoo calendar must never be deleted, even if the Google payload somehow marks the calendar as deleted.
+        """
+        google_calendars = GoogleCalendar([{
+            'id': 'primary-gid',
+            'summary': 'Primary',
+            'deleted': True,
+            'primary': True,
+        }])
+
+        self.organizer_user.calendar_ids.with_user(self.organizer_user)._sync_calendars_google2odoo(google_calendars)
+
+        self.assertTrue(
+            self.organizer_user.primary_calendar_id.exists(),
+            "Primary calendar must never be deleted even when flagged deleted on Google"
+        )
+
+    def test_g2o_updated_calendar_writes_changed_fields(self):
+        odoo_cal = self.create_calendar(name='Old Name', google_id='google-upd-1', need_sync=False)
+        google_calendars = GoogleCalendar([{'id': 'google-upd-1', 'summary': 'New Name From Google'}])
+
+        self.env['calendar.calendar'].with_user(self.organizer_user)._sync_calendars_google2odoo(google_calendars)
+
+        self.assertEqual(
+            odoo_cal.calendar_user_ids[0].label, 'New Name From Google',
+            "Calendar name should be updated from Google when Odoo has no pending changes",
+        )
+
+    def test_g2o_updated_calendar_skips_when_odoo_has_pending_changes(self):
+        odoo_cal = self.create_calendar(name='Local Name', google_id='gid', need_sync=True)
+        google_calendars = GoogleCalendar([{'id': 'gid', 'summary': 'Google Name'}])
+
+        self.env['calendar.calendar']._sync_calendars_google2odoo(google_calendars)
+
+        self.assertEqual(
+            odoo_cal.display_name, 'Local Name',
+            "Calendar with pending local changes should NOT be overwritten by Google",
+        )
+
+    @patch_api
+    @patch.object(ResUsers, '_sync_request')
+    def test_g2o_event_moved_to_a_different_calendar_in_google(self, mock_sync_request):
+        # Create an event in the primary calendar
+        google_id = '1'
+        event = self.env['calendar.event'].with_user(self.organizer_user).create({
+            'name': 'Hello world',
+            'start': date(2020, 1, 6),
+            'stop': date(2020, 1, 6),
+            'google_id': google_id,
+            'user_id': self.organizer_user.id,
+            'need_sync': False,
+            'partner_ids': [(6, 0, self.organizer_user.partner_id.ids)]
+        })
+
+        # Define mock return values for the '_sync_request' method.
+        def mock_sync(google_service, event_id=None, calendar=None):
+            if calendar.is_primary:
+                return {
+                    # Event was removed from the primary calendar
+                    'events': GoogleEvent([{'id': google_id, 'status': 'cancelled'}]),
+                    'default_reminders': (),
+                    'full_sync': False,
+                }
+            # Event respawned in a secondary calendar
+            elif calendar.google_id == self.secondary_calendar.google_id:
+                return {
+                    'events': GoogleEvent([{
+                        'id': google_id,
+                        'description': 'desc',
+                        "updated": datetime.now().replace(tzinfo=UTC).isoformat(),
+                        'organizer': {'email': 'odoocalendarref@gmail.com', 'self': True},
+                        'summary': 'Pricing new update',
+                        'visibility': 'public',
+                        'attendees': [],
+                        'reminders': {'useDefault': True},
+                        'start': {
+                            'dateTime': '2020-01-13T16:55:00+01:00',
+                            'timeZone': 'Europe/Brussels'
+                        },
+                        'end': {
+                            'dateTime': '2020-01-13T19:55:00+01:00',
+                            'timeZone': 'Europe/Brussels'
+                        },
+                    }]),
+                    'default_reminders': (),
+                    'full_sync': False,
+                }
+            raise Exception("Unexpected calendar")
+
+        mock_sync_request.side_effect = mock_sync
+        self.organizer_user.with_user(self.organizer_user)._sync_google_calendar(self.google_service)
+        self.assertTrue(event.exists(), "Event should not be deleted because of the move")
+        self.assertEqual(event.calendar_id, self.secondary_calendar)

addons/google_calendar/tests/test_sync_calendars_odoo2google.py
@@ -1,0 +1,126 @@
+from datetime import datetime
+from unittest.mock import patch
+
+from odoo.addons.google_calendar.models.res_users import ResUsers
+from odoo.addons.google_calendar.tests.test_sync_common import TestSyncGoogle, patch_api
+from odoo.addons.google_calendar.utils.google_event import GoogleEvent
+from odoo.tests.common import tagged
+
+
+@patch.object(ResUsers, '_get_google_calendar_token', lambda user: 'dummy-token')
+@tagged('at_install', '-post_install')
+class TestSyncCalendarsGoogle2Odoo(TestSyncGoogle):
+
+    @patch_api
+    def test_o2g_skip_when_sync_not_active(self):
+        """Nothing must be sent to Google when the user's sync status is not sync_active."""
+        self.organizer_user.pause_google_synchronization()
+        cal = self.create_calendar(name='My Calendar', need_sync=True)
+        cal._sync_calendars_odoo2google(self.google_service)
+        self.assertGoogleAPINotCalled()
+
+    @patch_api
+    def test_o2g_new_non_primary_calendar_calls_insert(self):
+        """Creating a new calendar must call _google_calendar_insert."""
+        cal = self.create_calendar(name='New Calendar')
+        self.assertGoogleCalendarInserted(cal._google_values())
+
+    @patch_api
+    def test_o2g_existing_synced_calendar_calls_patch(self):
+        """Editing synchronized calendar must call _google_calendar_patch."""
+        self.organizer_user.primary_calendar_id._sync_calendars_odoo2google(self.google_service)
+        self.organizer_user.primary_calendar_id.with_user(self.organizer_user).calendar_default_privacy = 'confidential'
+        self.assertGoogleCalendarPatched(
+            self.organizer_user.primary_calendar_id.id,
+            self.organizer_user.primary_calendar_id._google_values()
+        )
+
+    @patch_api
+    def test_o2g_moving_calendar_in_odoo_moves_event_in_google(self):
+        # Create an event in the primary calendar
+        google_id = '1'
+        event = self.env['calendar.event'].with_user(self.organizer_user).create({
+            'name': 'Hello world',
+            'google_id': google_id,
+            'user_id': self.organizer_user.id,
+            'need_sync': False,
+            'partner_ids': [(6, 0, self.organizer_user.partner_id.ids)]
+        })
+        # Move the event and assert that the move was called
+        event.calendar_id = self.secondary_calendar
+        self.assertGoogleEventMoved(google_id, "primary", self.secondary_calendar.google_id)
+        event.calendar_id = self.organizer_user.primary_calendar_id
+        self.assertGoogleEventMoved(google_id, self.secondary_calendar.google_id, "primary")
+
+    @patch_api
+    @patch.object(ResUsers, '_sync_request')
+    def test_o2g_moving_calendar_in_odoo_when_paused_moves_event_in_google_after_unpause(self, mock_sync_request):
+        google_id = '2'
+        event = self.env['calendar.event'].with_user(self.organizer_user).create({
+            'name': 'Hello world',
+            'google_id': google_id,
+            'user_id': self.organizer_user.id,
+            'need_sync': False,
+            'partner_ids': [(6, 0, self.organizer_user.partner_id.ids)]
+        })
+
+        # Define mock return values for the '_sync_request' method.
+        mock_sync_request.return_value = {
+            'events': GoogleEvent([]),
+            'default_reminders': (),
+            'full_sync': False,
+        }
+
+        # Pause the synchronization and move the event
+        self.organizer_user.sudo().pause_google_synchronization()
+        event.calendar_id = self.secondary_calendar
+
+        # With the synchronization paused, manually call the synchronization to simulate the page refresh.
+        self.organizer_user.sudo()._sync_google_calendar(self.google_service)
+        self.assertGoogleEventNotMoved()
+
+        # Unpause and call the calendar synchronization. Ensure the event was moved on Google side.
+        self.organizer_user.sudo().unpause_google_synchronization()
+        self.organizer_user.with_user(self.organizer_user).sudo()._sync_google_calendar(self.google_service)
+        self.assertGoogleEventMoved(google_id, "primary", self.secondary_calendar.google_id)
+
+    @patch_api
+    def test_o2g_changing_calendar_organizer_removes_and_recreates_calendar_in_google(self):
+        google_id = '3'
+        event = self.env['calendar.event'].with_user(self.organizer_user).create({
+            'name': 'Event',
+            'start': datetime(2020, 1, 15, 8, 0),
+            'stop': datetime(2020, 1, 15, 18, 0),
+            'google_id': google_id,
+            'user_id': self.organizer_user.id,
+            'need_sync': True,
+            'partner_ids': [(6, 0, self.organizer_user.partner_id.ids)]
+        })
+        insert_common_data = {
+            'start': {'dateTime': '2020-01-15T08:00:00+00:00', 'date': None},
+            'end': {'dateTime': '2020-01-15T18:00:00+00:00', 'date': None},
+            'summary': 'Event',
+            'description': event.description,
+            'location': '',
+            'guestsCanModify': True,
+            'extendedProperties': {'shared': {'%s_odoo_id' % self.env.cr.dbname: event.id}},
+            'reminders': {'overrides': [{'method': 'popup', 'minutes': 15}], 'useDefault': False},
+            'attendees': [{'email': 'o.o@example.com', 'responseStatus': 'accepted'}],
+            'conferenceData': None,
+            'transparency': 'opaque',
+        }
+        self.assertGoogleEventInserted({
+            'id': '3',
+            'organizer': {'email': 'o.o@example.com', 'self': True},
+            **insert_common_data,
+        })
+
+        google_id = event.google_id
+        event.user_id = self.attendee_user
+        self.assertGoogleEventDeleted(google_id)
+        self.assertFalse(event.google_id)
+        self.assertEqual(event.calendar_id, self.attendee_user.primary_calendar_id)
+
+        # Event should be inserted again
+        event._sync_odoo2google(self.google_service)
+        self.assertEqual(len(self._gsync_insert_values), 2)