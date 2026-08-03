stxavierhighschoolkmd-lab/Odoo-school1

[IMP] base_address_extended,contacts: update dependency
This commit update base_address_extended dependency to make
it independent of contacts app and move `Cities` menu in
contacts app and make it dependent on `base_address_extended`.

task-5208254

addons/base_address_extended/__manifest__.py
@@ -19,7 +19,7 @@ It is primarily used for EDIs that might need a special city code.
         'views/res_city_view.xml',
         'views/res_country_view.xml',
     ],
-    'depends': ['base', 'contacts'],
+    'depends': ['base'],
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
 }

addons/contacts/__manifest__.py
@@ -10,7 +10,7 @@
 This module gives you a quick view of your contacts directory, accessible from your home page.
 You can track your vendors, customers and other contacts.
 """,
-    'depends': ['base', 'mail'],
+    'depends': ['base', 'mail', 'base_address_extended'],
     'data': [
         'views/contact_views.xml',
     ],

LAIDOUDI33/LAID_ODOO

addons/base_address_extended/__manifest__.py
@@ -19,7 +19,7 @@ It is primarily used for EDIs that might need a special city code.
         'views/res_country_view.xml',
         'security/ir.access.csv',
     ],
-    'depends': ['base', 'contacts'],
+    'depends': ['web'],
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
 }

addons/contacts/__manifest__.py
@@ -10,7 +10,7 @@
 This module gives you a quick view of your contacts directory, accessible from your home page.
 You can track your vendors, customers and other contacts.
 """,
-    'depends': ['base', 'mail', 'web_hierarchy'],
+    'depends': ['base_address_extended', 'mail', 'web_hierarchy'],
     'data': [
         'views/contact_views.xml',
     ],