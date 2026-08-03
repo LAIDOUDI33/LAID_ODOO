odoo/odoo

[REF] bus: convert multi_tab_service service to plugin

odoo/upgrade_code/owl3-migration.py
@@ -1102,6 +1102,7 @@ MISC_WHITELIST = {
 SERVICES_MAPPING = {
     "bus.parameters": ['BusParametersPlugin', '@bus/bus_parameters_plugin'],
     "notification": ['NotificationPlugin', '@web/core/notifications/notification_plugin'],
+    "multi_tab": ['MultiTabPlugin', '@bus/multi_tab_service'],
     "worker_service": ['WorkerPlugin', '@bus/services/worker_plugin'],
 }
 

LAIDOUDI33/LAID_ODOO

odoo/upgrade_code/owl3-migration.py
@@ -1104,6 +1104,7 @@ SERVICES_MAPPING = {
     "discuss.upgrade": ['DiscussUpgradePlugin', '@mail/core/common/upgrade/upgrade_plugin'],
     "notification": ['NotificationPlugin', '@web/core/notifications/notification_plugin'],
     "mobile": ['MobilePlugin', '@web_mobile/js/mobile_plugin'],
+    "multi_tab": ['MultiTabPlugin', '@bus/multi_tab_plugin'],
     "sortable": ['SortablePlugin', '@web/core/util/sortable_plugin'],
     "worker_service": ['WorkerPlugin', '@bus/services/worker_plugin'],
 }