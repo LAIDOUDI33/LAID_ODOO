odoo/odoo

[FIX] portal: fix company address warning issue
Steps:
- Install sale app.
- Create SO for portal user.
- Login with portal user.
- Vat field is not editable and warning is wrong.

Issue:
- Before https://github.com/odoo/odoo/pull/211043 and recent fix https://github.com/odoo/odoo/pull/275207
portal user can edit their Vat number even if they have confirmed documents
(invoice or SO) if Vat field is not set. Since `is_company` refactoring
having set parent_name on address create related company and making
`is_commercial_address` False and because that `Vat` field became reaonly
and after recent fix `is_commercial_address` was set from `can_edit_vat`
and validation done based on `can_edit_vat` before that `Vat` was editable
if they have confirmed documents

Fix:
- Only make `Vat` readonly if Vat is set and is not individual address

addons/portal/tests/test_addresses.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from unittest.mock import patch
+
 from odoo.http import Request
 from odoo.tests import HttpCase, tagged
 from odoo.tests.common import JsonRpcException
@@ -166,6 +168,27 @@ class TestPortalAddresses(BaseCommon, HttpCase):
             self.portal_user.partner_id,
             [{**self.default_address_values, 'vat': 'BE0926372368'}],
         )
+
+    def test_vat_update_if_not_set(self):
+        self.authenticate(self.portal_user.login, self.portal_user.login)
+        csrf_token = Request.csrf_token(self)
+
+        with patch(
+            "odoo.addons.portal.models.res_partner.ResPartner.can_edit_vat",
+            return_value=False
+        ):
+            res = self._submit_address_values({
+                **self.default_address_values,
+                "vat": "BE0926372368",
+                "csrf_token": csrf_token,
+                "partner_id": self.portal_user.partner_id.id,
+            })
+            self.assertEqual(res, {"redirectUrl": "/my/addresses"})
+            # User should be able to update vat if not set
+            self.assertRecordValues(
+                self.portal_user.partner_id,
+                [{**self.default_address_values, "vat": "BE0926372368"}],
+            )
 
     def test_vat_update_with_parent_name(self):
         self.authenticate(self.portal_user.login, self.portal_user.login)

LAIDOUDI33/LAID_ODOO

X-original-commit: 54eab64d64b190e4fc16a07cf2327b82112394c7

addons/portal/tests/test_addresses.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from unittest.mock import patch
+
 from odoo.tests import HttpCase, tagged
 from odoo.tests.common import JsonRpcException
 from odoo.tools import mute_logger, urls
@@ -167,6 +169,27 @@ class TestPortalAddresses(BaseCommon, HttpCase):
             self.portal_user.partner_id,
             [{**self.default_address_values, 'vat': 'BE0926372368'}],
         )
+
+    def test_vat_update_if_not_set(self):
+        self.authenticate(self.portal_user.login, self.portal_user.login)
+        csrf_token = self.csrf_token()
+
+        with patch(
+            "odoo.addons.portal.models.res_partner.ResPartner._has_confirmed_documents",
+            return_value=True
+        ):
+            res = self._submit_address_values({
+                **self.default_address_values,
+                "vat": "BE0926372368",
+                "csrf_token": csrf_token,
+                "partner_id": self.portal_user.partner_id.id,
+            })
+            self.assertEqual(res, {"redirectUrl": "/my/addresses"})
+            # User should be able to update vat if not set
+            self.assertRecordValues(
+                self.portal_user.partner_id,
+                [{**self.default_address_values, "vat": "BE0926372368"}],
+            )
 
     def test_vat_update_with_parent_name(self):
         self.authenticate(self.portal_user.login, self.portal_user.login)