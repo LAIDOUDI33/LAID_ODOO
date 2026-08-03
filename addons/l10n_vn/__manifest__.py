odoo/odoo

[IMP] l10n_vn,l10n_vn_edi_viettel: move SInvoice symbols to l10n_vn
In this commit:

- Move the SInvoice symbol model and invoice symbol/number fields to l10n_vn so
  Vietnamese companies can manually manage SInvoice symbols without the
  Viettel module installed.

- User can create or edit symbols from symbol list view while keeping Fetch button
  available when Viettel is  installed.

task-6035788

addons/l10n_vn/__manifest__.py
@@ -30,8 +30,12 @@ This is the module to manage the accounting chart, bank information for Vietnam 
     'auto_install': ['account'],
     'data': [
         'data/account_tax_report_data.xml',
+        'security/ir.access.csv',
         'views/account_move_views.xml',
+        'views/account_journal_views.xml',
         'views/res_bank_views.xml',
+        'views/sinvoice_views.xml',
+        'views/res_config_settings_views.xml',
     ],
     'demo': [
         'demo/demo_company.xml',

addons/l10n_vn_edi_viettel/wizard/account_move_reversal.py
@@ -53,7 +53,7 @@ class AccountMoveReversal(models.TransientModel):
         # EXTEND 'account'
         for move in self.move_ids.filtered(lambda m: m._l10n_vn_edi_is_sent()):
             # If an invoice has a tax code (symbol starts with C) and the code has not been approved by the tax authorities, you cannot adjust/reverse it.
-            if move.l10n_vn_edi_invoice_symbol.name.startswith('C'):
+            if move.l10n_vn_sinvoice_symbol_id.name.startswith('C'):
                 invoice_lookup, _error_message = move._l10n_vn_edi_lookup_invoice()
                 if 'result' in invoice_lookup and invoice_lookup['result'][0].get('exchangeStatus') != 'INVOICE_HAS_CODE_APPROVED':
                     raise UserError(_('You cannot adjust/replace invoice %s, it has not been approved by the tax authorities.\n'

LAIDOUDI33/LAID_ODOO

addons/l10n_vn/__manifest__.py
@@ -30,8 +30,12 @@ This is the module to manage the accounting chart, bank information for Vietnam 
     'auto_install': ['account'],
     'data': [
         'data/account_tax_report_data.xml',
+        'security/ir.access.csv',
         'views/account_move_views.xml',
+        'views/account_journal_views.xml',
         'views/res_bank_views.xml',
+        'views/symbol_views.xml',
+        'views/res_config_settings_views.xml',
     ],
     'demo': [
         'demo/demo_company.xml',

addons/l10n_vn_edi_viettel/wizard/account_move_reversal.py
@@ -54,7 +54,7 @@ class AccountMoveReversal(models.TransientModel):
         # EXTEND 'account'
         for move in self.move_ids.filtered(lambda m: m._l10n_vn_edi_is_sent()):
             # If an invoice has a tax code (symbol starts with C) and the code has not been approved by the tax authorities, you cannot adjust/reverse it.
-            if move.l10n_vn_edi_invoice_symbol.name.startswith('C'):
+            if move.l10n_vn_symbol_id.name.startswith('C'):
                 access_token, error = move.company_id._l10n_vn_edi_get_access_token()
                 if error:
                     raise UserError(error)