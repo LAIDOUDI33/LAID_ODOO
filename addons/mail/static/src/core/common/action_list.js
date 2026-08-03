odoo/odoo

[IMP] mail,im_livechat: improve livechat and discuss UI/UX
This commit aims to improve the UI of livechat and discuss by removing
clouding icons.
It improves the UX by having the option to translate all new messages
when translating one message, this is a client side option meaning it
would need to be done again after refresh and is isolated to the current
channel.
We also improve the sort of canned responses by using the last used
canned function Odoo-wide (depends on other users as well) and being
able to download conversations even when the livechat is not ended.

task-6272656

addons/mail/static/src/core/common/action_list.js
@@ -1,5 +1,5 @@
 import { attClassObjectToString } from "@mail/utils/common/format";
-import { Component, onWillUnmount, props, t } from "@odoo/owl";
+import { Component, onWillDestroy, onWillUnmount, props, t } from "@odoo/owl";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { Action as ActionModel } from "@mail/core/common/action";
@@ -21,7 +21,7 @@ const actionListPropsSchema = {
     odooControlPanelSwitchStyle: t.boolean().optional(),
 };
 
-class Action extends Component {
+export class Action extends Component {
     static components = { Action, DropdownItem };
     static template = "mail.Action";
 
@@ -45,11 +45,15 @@ class Action extends Component {
         this.store = useService("mail.store");
         this.ui = useService("ui");
         this.attClassObjectToString = attClassObjectToString;
+        this.props.action.setRenderingContext(this);
         if (this.props.action.definition?.isMoreAction) {
             onWillUnmount(() => {
                 this.props.action.dropdownState.close();
             });
         }
+        onWillDestroy(() => {
+            this.props.action.unsetRenderingContext();
+        });
     }
 
     get action() {

LAIDOUDI33/LAID_ODOO

addons/mail/static/src/core/common/action_list.js
@@ -1,5 +1,5 @@
 import { attClassObjectToString } from "@mail/utils/common/format";
-import { Component, onWillUnmount, t, useProps } from "@odoo/owl";
+import { Component, onWillDestroy, onWillUnmount, t, useProps } from "@odoo/owl";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { Action as ActionModel } from "@mail/core/common/action";
@@ -21,7 +21,7 @@ const actionListPropsSchema = {
     odooControlPanelSwitchStyle: t.boolean().optional(),
 };
 
-class Action extends Component {
+export class Action extends Component {
     static components = { Action, DropdownItem };
     static template = "mail.Action";
 
@@ -45,11 +45,15 @@ class Action extends Component {
         this.store = useService("mail.store");
         this.ui = useService("ui");
         this.attClassObjectToString = attClassObjectToString;
+        this.props.action.setRenderingContext(this);
         if (this.props.action.definition?.isMoreAction) {
             onWillUnmount(() => {
                 this.props.action.dropdownState.close();
             });
         }
+        onWillDestroy(() => {
+            this.props.action.unsetRenderingContext();
+        });
     }
 
     get action() {